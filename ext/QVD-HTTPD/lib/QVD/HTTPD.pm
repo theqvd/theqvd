5d6e5e4208353d370547b343975711e6f85268fb
Salvador Fandino <salvador@qindel.com>
2009-08-31 13:10:49 +0000

    first steps through an HTTP server

--- ext/QVD-HTTPD/lib/QVD/HTTPD.pm
+++ ext/QVD-HTTPD/lib/QVD/HTTPD.pm
@@ -3,17 +3,96 @@ package QVD::HTTPD;
 use warnings;
 use strict;
 
+our $VERSION = '0.01';
+
+use parent qw(Net::Server);
+
+# token          = 1*<any CHAR except CTLs or separators>
+# separators     = "(" | ")" | "<" | ">" | "@"
+#                | "," | ";" | ":" | "\" | <">
+#                | "/" | "[" | "]" | "?" | "="
+#                | "{" | "}" | SP | HT
+my $token_re = qr/[!#\$%&'*+\-\.0-9a-zA-Z]+/;
+
+sub process_request {
+    my $self = shift;
+    while (<>) {
+	s/\r?\n$//;
+	next if /^\s*$/;
+	if (my ($method, $url) = m|^(\w+)\s+(.*?)\s+HTTP/1\.1$|) {
+	    my @headers;
+	    while(<>) {
+		s/\r?\n$//;
+		if (/^$/) {
+		    # end of headers
+		    $self->process_http_request($method, \@headers);
+		    last;
+		}
+		elsif (/^\s+(.*?)\s+$/) {
+		    # header continuation
+		    @headers or goto ERROR;
+		    $headers[-1] .= " " . $1;
+		}
+		elsif (my ($name, $value) = /^\s*($token_re)\s*:\s*(.*?)\s*$/o) {
+		    # new header
+		    push @headers, "${name}:${value}";
+		}
+		else {
+		    goto ERROR;
+		}
+	    }
+	}
+	else {
+	    goto ERROR;
+	}
+    }
+ OK:
+    print STDERR "connection closed\n";
+    return;
+
+ ERROR:
+    $self->send_http_error(500, "bad request",
+			   "bad HTTP request\n\nline:\n$_\n\n");
+    goto OK;
+}
+
+sub process_http_request {
+    my $self = shift;
+    my ($method, $headers) = @_;
+    use Data::Dumper;
+    my $text = Dumper \@_;
+    $self->send_http_response(200, 'Ok', ['Content-Type: text/plain'], $text);
+}
+
+sub send_http_response {
+    my $self = shift;
+    my $code = shift;
+    my $msg = shift;
+    my $headers = shift;
+    my $content = join('', @_);
+    print join("\r\n",
+	       "HTTP/1.1 $code $msg",
+	       @$headers,
+	       sprintf('Content-Length: %d', length $content),
+	       '',
+	       $content);
+}
+
+sub send_http_error {
+    my ($self, $code, $msg, $text) = @_;
+    $text =~ s/\r?\n/\r\n/g;
+    $self->send_http_request($code, $msg, ['Content-Type: text/plain'], $text);
+}
+
+__PACKAGE__->run(port => 8080);
+
+1;
+
+__END__
+
 =head1 NAME
 
 QVD::HTTPD - The great new QVD::HTTPD!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
 
 
 =head1 SYNOPSIS
@@ -27,30 +106,8 @@ Perhaps a little code snippet.
     my $foo = QVD::HTTPD->new();
     ...
 
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
 =head1 FUNCTIONS
 
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Salvador Fandiño, C<< <sfandino at yahoo.com> >>
 
 =head1 BUGS
 
@@ -58,50 +115,19 @@ Please report any bugs or feature requests to C<bug-qvd-httpd at rt.cpan.org>, o
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=QVD-HTTPD>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc QVD::HTTPD
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=QVD-HTTPD>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/QVD-HTTPD>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/QVD-HTTPD>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/QVD-HTTPD>
-
 =back
 
+=head1 AUTHORS
 
-=head1 ACKNOWLEDGEMENTS
-
+Salvador FandiE<ntilde>o (sfandino@yahoo.com)
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 Salvador Fandiño, all rights reserved.
+Copyright 2009 Qindel Consulting S.L., all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
 
-1; # End of QVD::HTTPD
+
