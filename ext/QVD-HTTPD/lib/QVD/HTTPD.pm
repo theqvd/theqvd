18c9b456c1e5b90f794790ac316f31b840ed5a2f
Salvador Fandino <salvador@qindel.com>
2009-09-03 11:03:00 +0000

    allow generation of responses without body, add HTTP method to request processor lookup algorithm

--- ext/QVD-HTTPD/lib/QVD/HTTPD.pm
+++ ext/QVD-HTTPD/lib/QVD/HTTPD.pm
@@ -5,9 +5,13 @@ use strict;
 
 our $VERSION = '0.01';
 
-use parent qw(Net::Server);
+use URI::Split qw(uri_split);
 
 use QVD::HTTP::StatusCodes qw(:all);
+
+use parent qw(Net::Server);
+
+
 
 # token          = 1*<any CHAR except CTLs or separators>
 # separators     = "(" | ")" | "<" | ">" | "@"
@@ -61,33 +65,35 @@ sub process_request {
 }
 
 sub set_http_request_processor {
-    my ($self, $plugin, $url) = @_;
+    my ($self, $callback, $method, $url) = @_;
     my $children_also = $url =~ s|/\*$||;
-    my $url_re = quotemeta($url);
-    $url_re .= "(?:/.*)?" if $children_also;
-    $url_re = qr/^$url_re$/;
+    my $matcher = quotemeta("$method $url");
+    $matcher .= "(?:/.*)?" if $children_also;
+    $matcher = qr/^$matcher$/;
     my $p = $self->{_http_request_processor} ||= [];
-    @$p = sort { length $_->[1] } @$p, [$plugin, $url, $url_re];
+    @$p = sort { length $_->[1] } @$p, [$callback, $url, $matcher];
     my $c = $self->{_http_request_processor_cache} ||= {};
-    delete $$c{$_} for (grep /$url_re/, keys %$c);
+    delete $$c{$_} for (grep /$matcher/, keys %$c);
     1
 }
 
 sub get_http_request_processor {
-    my $self = shift;
-    my $url = shift;
+    my ($self, $method, $url) = @_;
     my $c = $self->{_http_request_processor_cache} ||= {};
-    $c->{$url} ||= do {
+    my $pair = "$method $url";
+    $c->{$pair} ||= do {
 	my $p = $self->{_http_request_processor} ||= [];
-	my $h = (grep $url =~ $_->[2], @$p)[0]
+	my $h = (grep $pair =~ $_->[2], @$p)[0]
 	    or return undef;
+	$h->[0];
     }
 }
 
 sub process_http_request {
     my $self = shift;
     my ($method, $url, $headers) = @_;
-    my $processor = $self->get_http_request_processor($url);
+    my $path = (uri_split $url)[2];
+    my $processor = $self->get_http_request_processor($method, $path);
     if ($processor) {
 	$processor->($self, $method, $url, $headers);
     }
@@ -99,24 +105,30 @@ sub process_http_request {
 sub send_http_response {
     my $self = shift;
     my $code = int shift;
-    my $content_type = shift;
-    my $content = join('', @_);
     print join("\r\n",
 	       "HTTP/1.1 $code ". http_status_message($code),
-	       "Content-Type: $content_type",
-	       "Content-Length: " . length($content),
-	       '',
-	       $content);
+	       @_, '', '');
+}
+
+sub send_http_response_with_body {
+    my $self = shift;
+    my $code = shift;
+    my $content_type = shift;
+    my @headers = (ref $_[0] ? @{shift()} : ());
+    my $content = join('', @_);
+    $self->send_http_response($code,
+			      @headers,
+			      "Content-Type: $content_type",
+			      "Content-Length: " . length($content));
+    print $content;
 }
 
 sub send_http_error {
     my ($self, $code) = @_;
     $code ||= HTTP_INTERNAL_SERVER_ERROR;
     my $text = http_status_description($code);
-    $self->send_http_response($code, 'text/plain', $text);
+    $self->send_http_response_with_body($code, 'text/plain', $text);
 }
-
-__PACKAGE__->run(port => 8080);
 
 1;
 
