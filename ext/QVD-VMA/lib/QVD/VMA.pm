a6d95b8e0a13a003e80deff8c0a36aa343f8df21
Salvador Fandino <salvador@qindel.com>
2009-09-09 11:55:23 +0000

    boiler plate code generated by module-starter

---
+++ ext/QVD-VMA/lib/QVD/VMA.pm
@@ -0,0 +1,56 @@
+package QVD::VMA;
+
+our $VERSION = '0.01';
+
+use warnings;
+use strict;
+
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+QVD::VMA - The great new QVD::VMA!
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use QVD::VMA;
+
+    my $foo = QVD::VMA->new();
+    ...
+
+=head1 DESCRIPTION
+
+=head2 FUNCTIONS
+
+=over
+
+=item function1
+
+=item function2
+
+=back
+
+=head1 AUTHOR
+
+Salvador FandiE<ntilde>o (sfandino@yahoo.com)
+
+=head1 BUGS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright E<copy> 2009 Qindel Formacion y Servicios S.L., all rights
+reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
