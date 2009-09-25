a6a0026c6b326e7ec5dee3aaac644624707bee51
Joni Salonen <jsalonen@qindel.com>
2009-09-25 12:27:06 +0000

    Started implementation of simplerpc-interface for vmas.

--- ext/QVD-VMAS/lib/QVD/VMAS.pm
+++ ext/QVD-VMAS/lib/QVD/VMAS.pm
@@ -16,6 +16,91 @@ Version 0.01
 our $VERSION = '0.01';
 
 
+package QVD::VMAS::Impl;
+
+use parent 'QVD::SimpleRPC::Server';
+
+sub SimpleRPC_start_vm_listener {
+    my ($self, %params) = @_;
+    my $id = $params{id};
+    my $vma_port = 3030+$id;
+    my $agent_port = 5000+$id;
+    my $vma_client = QVD::VMA::Client->new('localhost', $vma_port);
+    if ($vma_client->start_vm_listener) {
+	return { host => 'localhost', 'port' => $agent_port };
+    }
+}
+
+sub SimpleRPC_start_vm {
+    my ($self, %params) = @_;
+    my $id = $params{id};
+
+    unless (defined $id) {
+	return { vm_status => 'aborted', error => 'invalid id: '.$id };
+    }
+    
+    my $schema = QVD::DB->new();
+    my $vm = $schema->resultset('VM')->find({id => $id});
+    unless (defined $vm) {
+	return { vm_status => 'aborted', error => 'invalid id: '.$id };
+    }
+    my $osi = $vm->osi;
+    unless (defined $osi) {
+	return { vm_status => 'aborted', error => 'no osi' };
+    }
+    
+    my $vma_port = 3030+$id;
+    my $agent_port = 5000+$id;
+    my $cmd = "kvm";
+    $cmd .= " -redir tcp:".$agent_port."::5000";
+    $cmd .= " -redir tcp:".$vma_port."::3030";
+    $cmd .= " -hda ".$osi->disk_image;
+    $cmd .= " -hdb ".$vm->storage if -e $vm->storage;
+    $cmd .= " -pidfile /var/run/qvd/vm-$id.pid";
+    $cmd .= " &";
+# FIXME executing a program in background doesn't fail even if program doesn't exist
+    system($cmd) == 0 or 
+    	return { vm_status => 'aborted', error => "Couldn't exec kvm: $!" };
+# Allow 2 seconds for generation of pid file
+    sleep 2;
+    my $pid = `cat /var/run/qvd/vm-$id.pid`;
+    if (kill 0, $pid) {
+	return { vm_status => 'starting' };
+    } else {
+# FIXME how to capture error message from kvm?
+	return { vm_status => 'aborted', error => 'vm exited' };
+    }
+}
+
+sub SimpleRPC_stop_vm {
+    my ($self, %params) = @_;
+    my $id = $params{id};
+    unless (defined $id) {
+	return { request => 'error', error => 'invalid id: '.$id };
+    }
+    
+    my $schema = QVD::DB->new();
+    my $vm = $schema->resultset('VM')->find({id => $id});
+    unless (defined $vm) {
+	return { request => 'error', error => 'invalid id: '.$id };
+    }
+
+    my $vma_port = 3030+$id;
+
+    my $vma_client = QVD::VMA::Client->new('localhost', $vma_port);
+    unless ($vma_client->is_connected) {
+	return { request => 'error', error => "Can't connect to VMA" };
+    }
+
+    my %r = $vma_client->poweroff;
+    if (defined $r{poweroff}) {
+	return { vm_status => 'stopping' };
+    } else {
+	return { request => 'error', error => "VMA can't poweroff vm" };
+    }
+}
+
+
 =head1 SYNOPSIS
 
 Quick summary of what the module does.
