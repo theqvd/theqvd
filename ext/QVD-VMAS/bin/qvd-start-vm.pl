4cca9917d50a61c31154d1df3680bc126f1ca5ca
Hugo Cornejo <hcornejo@qindel.com>
2009-10-22 11:02:22 +0000

    _is_kvm_running was renamed to is_vm_running in main module

--- ext/QVD-VMAS/bin/qvd-start-vm.pl
+++ ext/QVD-VMAS/bin/qvd-start-vm.pl
@@ -6,8 +6,7 @@ use QVD::VMAS;
 
 my $host_id = 1;
 my $vmas = QVD::VMAS->new();
-my @vms = $vmas->get_vms_for_host($host_id);
-my $vm = $vms[0];
+my $vm = $vmas->get_vms_for_host($host_id)->first;
 my $vm_id = $vm->vm_id;
 
 print "Starting VM $vm_id...\n";
@@ -15,8 +14,7 @@ my $r = $vmas->start_vm($vm);
 die "VM $vm_id is already running" if $r->{vm_status} eq 'started';
 die "Couldn't start VM $vm_id: $r->{error}" unless $r->{vm_status} eq 'starting';
 
-# FIXME Add a non-private method to check if the vm was started!
-$r = $vmas->_is_kvm_running($vm_id);
+$r = $vmas->is_vm_running($vm);
 die "VM $vm_id wasn't started" unless $r;
 
 for (;;) {
--- ext/QVD-VMAS/bin/qvd-stop-vm.pl
+++ ext/QVD-VMAS/bin/qvd-stop-vm.pl
@@ -6,8 +6,7 @@ use QVD::VMAS;
 
 my $host_id = 1;
 my $vmas = QVD::VMAS->new;
-my @vms = $vmas->get_vms_for_host($host_id);
-my $vm = $vms[0];
+my $vm = $vmas->get_vms_for_host($host_id)->first;
 my $vm_id = $vm->vm_id;
 print "Stopping VM $vm_id...\n";
 my $r = $vmas->stop_vm($vm);
@@ -15,7 +14,7 @@ die "Couldn't stop VM $vm_id: $r->{error}" unless $r->{request} eq 'success';
 
 for (;;) {
     print "Waiting for VM $vm_id to stop...\n";
-    last if not $vmas->_is_kvm_running($vm_id);
+    last if not $vmas->is_vm_running($vm);
     sleep 10;
 }
 print "VM $vm_id stopped.\n";
