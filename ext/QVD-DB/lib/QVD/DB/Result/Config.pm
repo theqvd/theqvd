82ae09c2f9d7047d545eb357e0ed0ccffd63b86d
Hugo Cornejo <hcornejo@qindel.com>
2009-10-27 14:56:04 +0000

    Value to 4K size

--- ext/QVD-DB/lib/QVD/DB/Result/Config.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/Config.pm
@@ -5,10 +5,10 @@ __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('configs');
 __PACKAGE__->add_columns(
 	key => {
-	    data_type => 'varchar(128)'
+	    data_type => 'varchar(64)'
 	},
 	value => {
-	    data_type => 'varchar(128)'
+	    data_type => 'varchar(4096)'
 	}
 	);
 __PACKAGE__->set_primary_key('key');
