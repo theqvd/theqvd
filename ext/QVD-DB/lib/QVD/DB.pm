224d99b11186782cd034a6919cc9ea2aba0436e2
Salvador Fandino <salvador@qindel.com>
2009-11-03 14:01:24 +0000

    full cleanup, use parent constructor to bless the object, use txn_do for transactions

--- ext/QVD-DB/lib/QVD/DB.pm
+++ ext/QVD-DB/lib/QVD/DB.pm
@@ -1,27 +1,60 @@
 package QVD::DB;
 
+our $VERSION = '0.01';
+
 use warnings;
 use strict;
 
-use parent qw/DBIx::Class::Schema/;
 use DBIx::Class::Exception;
-
 use Config::Tiny;
+
+use parent qw(DBIx::Class::Schema);
+
+__PACKAGE__->load_namespaces(result_namespace => 'Result');
+__PACKAGE__->exception_action(sub { warn @_ ; DBIx::Class::Exception::throw(@_);});
+
+sub new {
+    my $class = shift;
+    # Load database configuration from file
+    my $config = Config::Tiny->read('config.ini');
+    my $cdb = $config->{database};
+    my $conn_data_source = $cdb->{data_source};
+    my $conn_username = $cdb->{username};
+    my $conn_password = $cdb->{password};
+    $class->SUPER::connect($conn_data_source, $conn_username, $conn_password,
+				  { RaiseError => 1, AutoCommit => 0 });
+}
+
+sub erase {
+    my $db = shift;
+    for my $table (qw(osis
+		      vm_runtimes
+		      vms
+		      host_runtimes
+		      hosts
+		      users
+		      x_states
+		      vm_states
+		      user_states
+		      x_cmds
+		      vm_cmds
+		      user_cmds
+		      configs)) {
+
+	eval {
+	    $db->txn_do( sub { $_[1]->do("DROP TABLE $table CASCADE") } );
+	};
+	warn $@ if $@;
+    }
+}
+
+1;
+
+__END__
 
 =head1 NAME
 
 QVD::DB - The great new QVD::DB!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
-__PACKAGE__->load_namespaces(result_namespace => 'Result');
-__PACKAGE__->exception_action(sub { warn @_ ; DBIx::Class::Exception::throw(@_);});
 
 =head1 SYNOPSIS
 
@@ -34,144 +67,42 @@ Perhaps a little code snippet.
     my $foo = QVD::DB->new();
     ...
 
-=head1 EXPORT
+=head1 DESCRIPTION
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=cut
-
-sub new {
-    my $class = shift;
-    
-    my $config = Config::Tiny->new();
-    # Carga los parámetros necesarios desde el fichero de configuración
-    $config = Config::Tiny->read('config.ini');
-    my $conn_data_source = $config->{database}->{data_source};
-    my $conn_username = $config->{database}->{username};
-    my $conn_password = $config->{database}->{password};
-    
-    my @conn_info = ($conn_data_source, $conn_username, $conn_password, 
-    			{ RaiseError => 1, AutoCommit => 0 });
-    my $self = { conn_info => \@conn_info };
-    bless $self, $class;
-    $self->connect(@conn_info);
-}
-
-sub erase {
-    my $db = shift;
-    
-    eval { $db->storage->dbh->do("DROP TABLE osis CASCADE") };
-    warn $@ if $@;
-    $db->txn_commit;
-
-    eval { $db->storage->dbh->do("DROP TABLE vm_runtimes CASCADE") };
-    warn $@ if $@;
-    $db->txn_commit;
-
-    eval { $db->storage->dbh->do("DROP TABLE vms CASCADE") };
-    warn $@ if $@;
-    $db->txn_commit;
-    
-    eval { $db->storage->dbh->do('DROP TABLE host_runtimes CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;        
-
-    eval { $db->storage->dbh->do("DROP TABLE hosts CASCADE") };
-    warn $@ if $@;
-    $db->txn_commit;
-
-    eval { $db->storage->dbh->do('DROP TABLE users CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;
-
-    eval { $db->storage->dbh->do('DROP TABLE x_states CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;
-    
-    eval { $db->storage->dbh->do('DROP TABLE vm_states CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;
-    
-    eval { $db->storage->dbh->do('DROP TABLE user_states CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;
-    
-    eval { $db->storage->dbh->do('DROP TABLE x_cmds CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;    
-    
-    eval { $db->storage->dbh->do('DROP TABLE vm_cmds CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;
-    
-    eval { $db->storage->dbh->do('DROP TABLE user_cmds CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;    
-    
-    eval { $db->storage->dbh->do('DROP TABLE configs CASCADE') };
-    warn $@ if $@;
-    $db->txn_commit;   
-    
-}
-
-=head1 AUTHOR
-
-Joni Salonen, C<< <jsalonen at qindel.es> >>
-Nicolas Arenas, C<< <narenas at qindel.es> >> 
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-qvd-db at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=QVD-DB>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc QVD::DB
-
-
-You can also look for information at:
+=head2 API
 
 =over 4
 
-=item * RT: CPAN's request tracker
+=item $db = QVD::DB->new()
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=QVD-DB>
+Opens a new connection to the database using the configuration from
+the file 'config.ini'
 
-=item * AnnoCPAN: Annotated CPAN documentation
+=item $db->erase()
 
-L<http://annocpan.org/dist/QVD-DB>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/QVD-DB>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/QVD-DB>
+Drops all the database tables.
 
 =back
 
+=head1 AUTHORS
 
-=head1 ACKNOWLEDGEMENTS
+Joni Salonen (jsalonen at qindel.es)
 
+Nicolas Arenas (narenas at qindel.es)
+
+Salvador FandiE<ntilde>o (sfandino@yahoo.com)
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-qvd-db at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=QVD-DB>.  I will be
+notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 Joni Salonen, all rights reserved.
+Copyright 2009 Qindel Formacion y Servicios S.L., all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of QVD::DB
