272b946ed13222559e89f16bd355e5ce0e473462
Hugo Cornejo <hcornejo@qindel.com>
2009-11-11 15:50:34 +0000

    GUI client now masks the password entry and has labeled fields

--- ext/QVD-Client/bin/qvd-gui-client.pl
+++ ext/QVD-Client/bin/qvd-gui-client.pl
@@ -8,6 +8,7 @@ package MyFrame;
 use base 'Wx::Frame';
 # import the event registration function
 use Wx::Event qw(EVT_BUTTON);
+use Wx qw(:everything);
 
 sub new {
     my $ref = shift;
@@ -15,7 +16,9 @@ sub new {
                                  -1,              # ID -1 means any
                                  'QVD',           # title
                                  [-1, -1],        # default position
-                                 [310, 190],      # size
+                                 [370, 190],      # size
+				 wxCAPTION
+
                                  );
 
     my $panel = Wx::Panel->new( $self,            # parent window
@@ -25,40 +28,60 @@ sub new {
     my $username = Wx::TextCtrl->new( $panel,     # parent window
                                   -1,             # ID
                                   'qvd',     # label
-                                  [20, 20],       # position
-                                  [270, -1],      # default size
+                                  [110, 20],       # position
+                                  [240, -1],      # default size
                                   );
     $self->{username}=$username;
+    Wx::StaticText->new( $panel,     # parent window
+			  -1,             # ID
+			  'Usuario',     # label
+			  [20, 25],       # position
+			  [270, -1],      # default size
+			  );
+
 				  
     my $password = Wx::TextCtrl->new( $panel,     # parent window
                                   -1,             # ID
                                   'passw0rd',     # label
-                                  [20, 60],       # position
-                                  [270, -1],      # default size
+                                  [110, 60],       # position
+                                  [240, -1],      # default size
+				  wxTE_PASSWORD, 
                                   );
     $self->{password}=$password;
+    Wx::StaticText->new( $panel,     # parent window
+		      -1,             # ID
+		      'Contraseña',     # label
+		      [20, 65],       # position
+		      [270, -1],      # default size
+		      );
 
     my $host = Wx::TextCtrl->new( $panel,         # parent window
                                   -1,             # ID
                                   'aguila',       # label
-                                  [20, 100],      # position
-                                  [200, -1],      # default size
+                                  [110, 100],      # position
+                                  [180, -1],      # default size
                                   );
     $self->{host}=$host;
 				  
     my $port = Wx::TextCtrl->new( $panel,         # parent window
                                   -1,             # ID
                                   '8080',         # label
-                                  [230, 100],     # position
-                                  [60, -1],       # default size
+                                  [300, 100],     # position
+                                  [50, -1],       # default size
                                   );	
     $self->{port}=$port;
+    Wx::StaticText->new( $panel,     # parent window
+		      -1,             # ID
+		      'Servidor',     # label
+		      [20, 105],       # position
+		      [270, -1],      # default size
+		      );
 				  
     my $button = Wx::Button->new( $panel,         # parent window
                                   -1,             # ID
                                   'Conectar',     # label
                                   [20, 140],      # position
-                                  [270, -1],      # default size
+                                  [330, -1],      # default size
                                   );									    
 				  
 				  
