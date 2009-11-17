75c3a9399c2263de4738785208ed79ad15ee5737
Joni Salonen <jsalonen@qindel.com>
2009-11-17 12:45:59 +0000

    bugfix - add_unique_constraint needs arraref

--- ext/QVD-DB/lib/QVD/DB/Result/OSI.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/OSI.pm
@@ -23,7 +23,7 @@ __PACKAGE__->add_columns(
 	}
 	);
 __PACKAGE__->set_primary_key('id');
-__PACKAGE__->add_unique_constraint('name');
+__PACKAGE__->add_unique_constraint(['name']);
 __PACKAGE__->has_many(vms => 'QVD::DB::Result::VM', 'osi_id');
 
 1;
