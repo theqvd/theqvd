f3c39225456f853a28cef060b7205985cc10a032
Salvador Fandino <salvador@qindel.com>
2009-12-03 16:10:13 +0000

    default to port 8443 for a SSL connection

--- ext/QVD-Client/bin/qvd-client.pl
+++ ext/QVD-Client/bin/qvd-client.pl
@@ -8,15 +8,13 @@ use QVD::HTTP::StatusCodes qw(:status_codes);
 use IO::Socket::Forwarder qw(forward_sockets);
 use MIME::Base64 qw(encode_base64);
 
-$IO::Socket::Forwarder::debug = 1;
-
 # Forces a flush
 $| = 1;
 
 my $username = shift @ARGV;
 my $password = shift @ARGV;
 my $host = shift @ARGV;
-my $port = shift @ARGV // "8080";
+my $port = shift @ARGV // "8443";
 
 my $authorization = 'Basic '.encode_base64("$username:$password", '');
 
@@ -43,7 +41,7 @@ while (1) {
 	    or die "connection from nxproxy failed";
 	undef $ll; # close the listening socket
 	my $s2 = $httpc->get_socket;
-	forward_sockets($s1, $s2);
+	forward_sockets($s1, $s2); # , debug => 1);
 	last;
     }
     elsif ($code >= 100 and $code < 200) {
