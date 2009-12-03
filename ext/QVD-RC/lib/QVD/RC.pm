29a7d221622c48dc52fa7a2c3955bc84a2762983
Salvador Fandino <salvador@qindel.com>
2009-12-03 13:49:08 +0000

    RC code moved from frontend plugin

--- ext/QVD-RC/lib/QVD/RC.pm
+++ ext/QVD-RC/lib/QVD/RC.pm
@@ -1,107 +1,100 @@
 package QVD::RC;
 
+our $VERSION = '0.01';
+
 use warnings;
 use strict;
 
+use QVD::Config;
+
+use Log::Log4perl qw(:easy);
+Log::Log4perl::init('log4perl.conf');
+
+use parent qw(QVD::HTTPD);
+
+
+sub post_configure_hook {
+    my $server = shift;
+    my $impl = QVD::Frontend::Plugin::RC::Impl->new();
+    $impl->set_http_request_processors($server, '/qvd/rc/*');
+}
+
+package QVD::RC::Impl;
+
+use parent 'QVD::SimpleRPC::Server';
+
+sub _get_hkd_pid {
+    my $pid = undef;
+    # FIXME: get pid from database, or at least make the filename
+    # something configurable
+    my $hkd_pid_file = '/var/run/qvd/hkd.pid';
+    $pid =  `cat $hkd_pid_file` if (-f $hkd_pid_file);
+    chomp $pid;
+    $pid;
+}
+
+sub SimpleRPC_ping_hkd {
+    my $self = shift;
+    my $pid = _get_hkd_pid();
+    my $result = undef;
+    $result = kill('USR1', $pid) if $pid;
+    if ($result) {
+	return { request => 'success' };
+    } else {
+	my $msg = $pid ? $! : 'Not running';
+	return { request => 'error', error => 'Sending signal to HKD: '.$msg };
+    }
+}
+
+1;
+
+__END__
+
 =head1 NAME
 
-QVD::RC - The great new QVD::RC!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+QVD::RC - Remote control service
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+  use QVD::RC;
+  QVD::RC->set_http_request_processors($httpd, $base_url);
 
-Perhaps a little code snippet.
 
-    use QVD::RC;
+=head1 DESCRIPTION
 
-    my $foo = QVD::RC->new();
-    ...
+Webservice to signal local processes
 
-=head1 EXPORT
+=head2 RPC API
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+The RC service accepts the following RPC calls.
 
-=head1 FUNCTIONS
+=over
 
-=head2 function1
+=item ping_hkd
 
-=cut
+Pings the house-keeping daemon on this host by sending it the USR1 signal.
 
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+=back
 
 =head1 AUTHOR
 
+Joni Salonen, C<< <jsalonen at qindel.com> >>
 Salvador Fandiño, C<< <sfandino at yahoo.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-qvd-rc at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=QVD-RC>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc QVD::RC
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=QVD-RC>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/QVD-RC>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/QVD-RC>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/QVD-RC>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
+Please report any bugs or feature requests to C<bug-qvd-rc at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=QVD-RC>.  I will be
+notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
 
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 Salvador Fandiño, all rights reserved.
+Copyright 2009 Qindel Formacion y Servicios S.L., all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
-
-1; # End of QVD::RC
