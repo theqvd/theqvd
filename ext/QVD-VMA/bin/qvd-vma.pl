ed43add9c543884bf26948f2eacea7751b90ee37
Joni Salonen <jsalonen@qindel.com>
2010-03-04 17:34:19 +0000

    The run directory has to exist

--- ext/QVD-VMA/bin/qvd-vma.pl
+++ ext/QVD-VMA/bin/qvd-vma.pl
@@ -7,6 +7,8 @@ use App::Daemon qw/daemonize/;
 use QVD::VMA;
 use QVD::VMA::Config;
 
+my $rundir = cfg('vma.run_dir', '/var/run/qvd');
+-d $rundir or mkdir $rundir;
 $App::Daemon::pidfile = cfg('vma.pid_file', '/var/run/qvd/vma.pid');
 $App::Daemon::logfile = cfg('vma.log_file', '/var/log/qvd/vma.log');
 $App::Daemon::as_user = 'root';
