00492311d247f8a9b8844801af4a8161e2376cfd
Joni Salonen <jsalonen@qindel.com>
2010-03-22 14:21:28 +0000

    Documented qvd-rc so that a man page could be made - #338

--- ext/QVD-RC/bin/qvd-rcd.pl
+++ ext/QVD-RC/bin/qvd-rcd.pl
@@ -24,3 +24,69 @@ daemonize;
 # FIXME: make listening port configurable via DB
 my $rc = QVD::RC->new(port => 8080);
 $rc->run();
+
+__END__
+
+=head1 NAME
+
+qvd-rc - The QVD remote control service
+
+=head1 SYNOPSIS
+
+qvd-rc -h
+
+qvd-rc start [-X]
+
+qvd-rc stop
+
+qvd-rc status
+
+=head1 DESCRIPTION
+
+B<qvd-rc> can be used to start and stop the QVD remote control service, and to check if
+it's running. The QVD remote control is a network service for the QVD
+house-keeping daemon.
+
+=head1 OPTIONS
+
+=over
+
+=item -h
+
+Print the usage and exit.
+
+=item -X
+
+Start in foreground and log to screen. Useful for debugging.
+
+=back
+
+=head1 FILES
+
+=over
+
+=item F</etc/qvd/config.ini>
+
+The main configuration file.
+
+=item F</etc/qvd/log4perl.conf>
+
+The configuration for Log4perl, the logging framework.
+
+=item F</var/run/qvd/rc.pid>
+
+The file that stores the process id of the daemon. The path can be configured
+in F<config.ini>.
+
+=back
+
+=head1 SEE ALSO
+
+L<qvd-hkd(1)> - The QVD house-keeping daemon.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by Qindel Formacion y Servicios S.L.
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
