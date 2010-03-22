c7b05331e2aa17c616c9475868c8390e6e47694e
Hugo Cornejo <hcornejo@qindel.com>
2010-03-22 15:10:03 +0000

    Man documentation for L7R

--- ext/QVD-L7R/bin/qvd-l7rd.pl
+++ ext/QVD-L7R/bin/qvd-l7rd.pl
@@ -39,3 +39,63 @@ write_file('certs/server-key.pem', $server_key);
 daemonize;
 my $l7r = QVD::L7R->new(port => 8443, SSL => 1);
 $l7r->run();
+
+__END__
+
+=head1 NAME
+
+qvd-l7rd - The QVD Level 7 Router
+
+=head1 SYNOPSIS
+
+qvd-l7rd -h
+
+qvd-l7rd start [-X] 
+
+qvd-l7rd stop
+
+qvd-l7rd status
+
+=head1 DESCRIPTION
+
+B<qvd-l7rd> is a daemon that runs the QVD level 7 router. It can be used
+to start and stop the router, and to check if it is running.
+
+=head1 OPTIONS
+
+=over
+
+=item -h
+
+Print usage and exit.
+
+=item -X 
+
+Start the L7R in foreground and log messages to screen. Useful for debugging.
+
+=back
+
+=head1 FILES
+
+=over 
+
+=item F</etc/qvd/log4perl.conf>
+
+The configuration file for Log4perl, the logging framework.
+
+=item F</var/run/qvd/l7r.pid>
+
+The file where the L7R pid is stored. The path can be configured in "l7r_pid_file" config entry.
+
+=item F</var/log/qvd.log>
+
+The log file. The path can be configured in "l7r_log_file" config entry.
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by Qindel Formacion y Servicios S.L., 
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
