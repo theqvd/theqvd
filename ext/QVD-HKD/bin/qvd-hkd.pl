c0034257b89cfd8d86f2a8039ad2c829090c1f7c
Joni Salonen <jsalonen@qindel.com>
2010-03-22 17:07:22 +0000

    Added POD for creating man page in hkd - #337

--- ext/QVD-HKD/bin/qvd-hkd.pl
+++ ext/QVD-HKD/bin/qvd-hkd.pl
@@ -21,6 +21,71 @@ $App::Daemon::as_user = "root";
 
 daemonize;
 my $hkd = QVD::HKD->new(loop_wait_time => $POLL_TIME);
-$hkd->run;
 
 
+__END__
+
+=head1 NAME
+
+qvd-hkd - The QVD house-keeping daemon
+
+=head1 SYNOPSIS
+
+qvd-hkd -h
+
+qvd-hkd start [-X] 
+
+qvd-hkd stop
+
+qvd-hkd status
+
+=head1 DESCRIPTION
+
+The QVD house-keeping daemon monitors the QVD database for commands it has to
+perform. It also maintains the states of the virtual machines assinged to its
+host consistent with the database. The B<qvd-hkd> command is used to start and
+stop the HKD, and to check if it is running.
+
+=head1 OPTIONS
+
+=over
+
+=item -h
+
+Print usage and exit.
+
+=item -X 
+
+Start the HKD in foreground and log messages to screen. Useful for debugging.
+
+=back
+
+=head1 FILES
+
+=over 
+
+=item F</etc/qvd/config.ini>
+
+The main configuration file. 
+
+=item F</etc/qvd/log4perl.conf>
+
+The configuration file for Log4perl, the logging framework.
+
+=item F</var/run/qvd/hkd.pid>
+
+The file where the HKD pid is stored. The path can be configured in the QVD
+database.
+
+=item F</var/log/qvd.log>
+
+The log file. The path is configured in the QVD database.
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by Qindel Formacion y Servicios S.L., 
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
