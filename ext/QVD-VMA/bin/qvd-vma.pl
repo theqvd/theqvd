cf2eb4e6509143e64e4c07b9e3001e1c432135ca
Joni Salonen <jsalonen@qindel.com>
2010-03-22 11:09:21 +0000

    Documented qvd-vma so that a man page could be made - see #335.

--- ext/QVD-VMA/bin/qvd-vma.pl
+++ ext/QVD-VMA/bin/qvd-vma.pl
@@ -19,3 +19,67 @@ Log::Log4perl::init('log4perl.conf');
 daemonize();
 my $vma = QVD::VMA->new(port => 3030);
 $vma->run();
+
+__END__
+
+=head1 NAME
+
+qvd-vma - The QVD Virtual Machine Agent
+
+=head1 SYNOPSIS
+
+qvd-vma -h
+
+qvd-vma start [-X] 
+
+qvd-vma stop
+
+qvd-vma status
+
+=head1 DESCRIPTION
+
+B<qvd-vma> is a daemon that runs the QVD virtual machine agent. It can be used
+to start and stop the agent, and to check if it is running.
+
+=head1 OPTIONS
+
+=over
+
+=item -h
+
+Print usage and exit.
+
+=item -X 
+
+Start the VMA in foreground and log messages to screen. Useful for debugging.
+
+=back
+
+=head1 FILES
+
+=over 
+
+=item F</etc/qvd/vma.ini>
+
+The main configuration file. 
+
+=item F</etc/qvd/log4perl.conf>
+
+The configuration file for Log4perl, the logging framework.
+
+=item F</var/run/qvd/vma.pid>
+
+The file where the VMA pid is stored. The path can be configured in F<vma.ini>.
+
+=item F</var/log/qvd/vma.log>
+
+The log file. The path can be configured in F<vma.ini>.
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by Qindel Formacion y Servicios S.L., 
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
