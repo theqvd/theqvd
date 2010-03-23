5d9069df23478b8c09b7fd87316cfdef4cbcc179
Joni Salonen <jsalonen@qindel.com>
2010-03-23 17:29:55 +0000

    Initial POD for qvd-admin man page - see #334

--- ext/QVD-Admin/bin/qvd-admin.pl
+++ ext/QVD-Admin/bin/qvd-admin.pl
@@ -20,3 +20,54 @@ my @args = @ARGV;
 my $admin = QVD::AdminCLI->new($quiet);
 $admin->set_filter($filter) if $filter;
 $admin->dispatch_command($object, $command, $help, @args);
+
+__END__
+
+=head1 NAME
+
+qvd-admin - The QVD administration tool
+
+=head1 SYNOPSIS
+
+qvd-admin I<object> I<command> [I<options>] [I<arguments>]
+
+=head1 DESCRIPTION
+
+B<qvd-admin> is the command line tool for administrating a QVD installation.
+The basic interface is structured around the idea of performing commands to
+objects that the QVD platform manages. The objects are things like virtual
+machines, virtualization hosts, operating system images (OSI) and users.
+
+Run 'qvd-admin -h' and 'qvd-admin I<object> help' to access the online
+documentation.
+
+=head1 COMMON OPTIONS
+
+=over
+
+=item -h
+
+Print the help text and exit.
+
+=item -f I<filter>
+
+Apply the command only to the objects matched by the given filter.
+
+=back
+
+=head1 FILES
+
+=over
+
+=item F</etc/qvd/config.ini>
+
+The main configuration file.
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by Qindel Formacion y Servicios S.L., 
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
