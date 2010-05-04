50e926c8c5f85abcf5f2edd679032818a8033ca9
Joni Salonen <jsalonen@qindel.com>
2010-05-04 14:49:42 +0000

    Initial implementation of QVD::Log for #395

--- ext/QVD-Log/lib/QVD/Log.pm
+++ ext/QVD-Log/lib/QVD/Log.pm
@@ -3,100 +3,49 @@ package QVD::Log;
 use warnings;
 use strict;
 
+our $VERSION = '0.01';
+
+my %config = (
+    'log4perl.rootLogger' => 'DEBUG, LOGFILE',
+    'log4perl.appender.LOGFILE' => 'Log::Log4perl::Appender::File',
+    'log4perl.appender.LOGFILE.filename' => '/var/log/qvd.log',
+    'log4perl.appender.LOGFILE.mode' => 'append',
+    'log4perl.appender.LOGFILE.layout' => 'PatternLayout',
+    'log4perl.appender.LOGFILE.layout.ConversionPattern' => '%d %P %F %L %c - %m%n',
+);
+
+print "Init'ing Log::Log4perl\n";
+
+Log::Log4perl::init_once(%config);
+
+1;
+
+__END__
+
 =head1 NAME
 
-QVD::Log - The great new QVD::Log!
+QVD::Log - QVD logging utilities
 
 =head1 VERSION
 
 Version 0.01
 
-=cut
-
-our $VERSION = '0.01';
-
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Initializes Log::Log4perl with QVD configuration. For example:
 
-Perhaps a little code snippet.
-
+    use Log::Log4perl qw(:easy :levels);
     use QVD::Log;
 
-    my $foo = QVD::Log->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+    INFO "Logging works!";
 
 =head1 AUTHOR
 
 QVD Team, C<< <qvd at qindel.com> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-qvd-log at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=QVD-Log>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc QVD::Log
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=QVD-Log>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/QVD-Log>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/QVD-Log>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/QVD-Log/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2010 QVD Team.
+Copyright 2010 Qindel Formacion y Servicios S.L.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -106,13 +55,8 @@ any later version.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU General Public License 2 for more details.
 
 A copy of the GNU General Public License is available in the source tree;
 if not, write to the Free Software Foundation, Inc.,
 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
-
-=cut
-
-1; # End of QVD::Log
