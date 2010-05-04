de89f21598ea16c042f70293b01d18c568244cd2
Joni Salonen <jsalonen@qindel.com>
2010-05-04 16:23:03 +0000

    Log4perl seems to expect hashref instead of a hash - contrary to the documentation. Also, "use Log::Log4perl" is mandatory...

--- ext/QVD-Log/lib/QVD/Log.pm
+++ ext/QVD-Log/lib/QVD/Log.pm
@@ -1,22 +1,23 @@
 package QVD::Log;
 
+use Log::Log4perl;
+
 use warnings;
 use strict;
 
 our $VERSION = '0.01';
 
-my %config = (
+my $config = {
     'log4perl.rootLogger' => 'DEBUG, LOGFILE',
     'log4perl.appender.LOGFILE' => 'Log::Log4perl::Appender::File',
     'log4perl.appender.LOGFILE.filename' => '/var/log/qvd.log',
     'log4perl.appender.LOGFILE.mode' => 'append',
     'log4perl.appender.LOGFILE.layout' => 'PatternLayout',
     'log4perl.appender.LOGFILE.layout.ConversionPattern' => '%d %P %F %L %c - %m%n',
-);
+};
 
 print "Init'ing Log::Log4perl\n";
-
-Log::Log4perl::init_once(%config);
+Log::Log4perl::init_once($config);
 
 1;
 
