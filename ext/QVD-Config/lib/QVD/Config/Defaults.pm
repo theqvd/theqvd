b45060fc4c6df1e867de93c912da71e01d6bd7b9
Salvador Fandino <salvador@qindel.com>
2010-05-12 15:55:31 +0000

    centralized defaults

---
+++ ext/QVD-Config/lib/QVD/Config/Defaults.pm
@@ -0,0 +1,57 @@
+package QVD::Config;
+
+use Config::Properties;
+
+our $defaults = Config::Properties->new;
+$defaults->load(*DATA);
+
+1;
+
+__DATA__
+
+# nodename = blas
+
+database.host = localhost
+database.user = qvd
+database.name = qvd
+# database.password = passw0rd
+
+path.run = /var/run
+path.log = /var/log
+path.storage.root = /var/lib/qvd/storage
+path.storage.staging = ${path.storage.root}/staging
+path.storage.images = ${path.storage.root}/images
+path.storage.overlays = ${path.storage.root}/overlays
+path.storage.homes = ${path.storage.root}/homes
+
+command.kvm = kvm
+command.kvm-img = kvm-img
+
+l7r.as_user = qvd
+l7r.pid_file = ${path.run}/qvd/l7r.pid
+l7r.auth.mode = basic
+
+# l7r.auth.mode = ldap
+# l7r.auth.ldap.host =
+# l7r.auth.ldap.base =
+
+hkd.as_user = qvd
+hkd.pid_file = ${path.run}/qvd/hkd.pid
+
+wat.admin.login = admin
+# wat.admin.password = foobar
+
+log.filename = ${path.log}/qvd.log
+log.level = INFO
+
+# internal parameters, do not change!!!
+internal.l7r.poll_time = 5
+internal.l7r.timeout.vm_start = 270
+internal.l7r.timeout.vma = 4
+
+internal.hkd.timeout.state.starting = 240
+internal.hkd.timeout.state.stopping_1 = 30
+internal.hkd.timeout.state.stopping_2 = 240
+internal.hkd.timeout.state.zombie_1 = 30
+internal.hkd.timeout.state.running = 120
+internal.hkd.timeout.vma = 4
