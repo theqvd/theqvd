5968c1d49d56ecce27fe46dd1205ad7fd05defc8
Salvador Fandino <salvador@qindel.com>
2010-05-13 11:04:33 +0000

    require --force if database already deployed

--- ext/QVD-Admin/bin/qvd-deploy-db.pl
+++ ext/QVD-Admin/bin/qvd-deploy-db.pl
@@ -3,6 +3,20 @@
 use strict;
 use warnings;
 
+use Getopt::Long;
+my $force;
+GetOptions("force|f" => \$force) or exit (1);
+
 use QVD::DB::Simple;
+
+unless ($force) {
+    for (qw(VM VM_Runtime Host Osi User)) {
+        eval { rs($_)->search->count };
+        unless ($@ =~ /relation\s+"(\w+)"\+does\+not\+exist/) {
+            die "Database already contains table $_, use '--force' to redeploy the database\n";
+        }
+    }
+}
+
 db->deploy({add_drop_table => 1});
 
