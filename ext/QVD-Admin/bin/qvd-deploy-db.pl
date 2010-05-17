bc141d9a5cfb11a7026163492e59d09a899ed651
Hugo Cornejo <hcornejo@qindel.com>
2010-05-17 14:09:53 +0000

    Solves ticket #417

--- ext/QVD-Admin/bin/qvd-deploy-db.pl
+++ ext/QVD-Admin/bin/qvd-deploy-db.pl
@@ -7,15 +7,19 @@ use Getopt::Long;
 my $force;
 GetOptions("force|f" => \$force) or exit (1);
 
+use QVD::DB;
 use QVD::DB::Simple;
 
 unless ($force) {
-    for (qw(VM VM_Runtime Host Osi User)) {
-        eval { rs($_)->search->count };
-        unless ($@ =~ /relation\s+"(\w+)"\+does\+not\+exist/) {
-            die "Database already contains table $_, use '--force' to redeploy the database\n";
-        }
+    
+    my $rs = QVD::DB->new();
+    my $dbh = $rs->storage->dbh;
+    eval { $dbh->do("select count(*) from configs;"); };
+    
+    unless ($@) {
+	die "Database already contains QVD tables, use '--force' to redeploy the database\n";
     }
+    
 }
 
 db->deploy({add_drop_table => 1});
