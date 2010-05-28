cf50c209f560b73c3243c9d03cf923a0a930a8a1
Salvador Fandino <salvador@qindel.com>
2010-05-28 14:20:42 +0000

    properties for OSIs

--- ext/QVD-DB/lib/QVD/DB/Result/OSI.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/OSI.pm
@@ -3,31 +3,20 @@ use base qw/DBIx::Class/;
 
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('osis');
-__PACKAGE__->add_columns(
-	id => {
-	    data_type => 'integer',
-	    is_auto_increment => 1
-	},
-	name => {
-	    data_type => 'varchar(64)'
-	},
-	memory => {
-	    data_type => 'integer',
-	},
-	user_storage_size => {
-	    data_type => 'integer',
-	    is_nullable => 1,
-	},
-	use_overlay => {
-	    data_type => 'boolean'
-	},
-	disk_image => {
-	# Valor tomado de la variable PATH_MAX de /usr/src/linux-headers-2.6.28-15/include/linux/limits.h
-	    data_type => 'varchar(4096)'
-	}
-	);
+__PACKAGE__->add_columns( id          => { data_type => 'integer',
+                                           is_auto_increment => 1 },
+                          name        => { data_type => 'varchar(64)' },
+                          memory      => { data_type => 'integer' },
+                          use_overlay => { data_type => 'boolean' },
+                          # Valor tomado de la variable PATH_MAX de
+                          # /usr/src/linux-headers-2.6.28-15/include/linux/limits.h:
+                          disk_image  => { data_type => 'varchar(4096)' },
+                          user_storage_size => { data_type => 'integer',
+                                                 is_nullable => 1 } );
+
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint(['name']);
 __PACKAGE__->has_many(vms => 'QVD::DB::Result::VM', 'osi_id');
-
+__PACKAGE__->has_many(properties => 'QVD::DB::Result::OSI_Property',
+                      'host_id', { join_type => 'INNER' });
 1;
