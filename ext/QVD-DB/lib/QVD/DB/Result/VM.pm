2027c3a556aa825d3091ce79b0b101154591de29
Salvador Fandino <salvador@qindel.com>
2010-06-01 12:32:39 +0000

    code layout

--- ext/QVD-DB/lib/QVD/DB/Result/VM.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/VM.pm
@@ -3,31 +3,16 @@ use base qw/DBIx::Class/;
 
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('vms');
-__PACKAGE__->add_columns(
-	id => {
-	    data_type => 'integer',
-	    is_auto_increment => 1
-	},
-	name => {
-	    data_type => 'varchar(64)'
-	},
-	user_id => {
-	    data_type => 'integer',
-	    is_auto_increment => 1
-	},
-	osi_id => {
-	    data_type => 'integer',
-	    is_auto_increment => 1
-	},
-	ip => {
-	    data_type => 'varchar(15)'
-	},
-	storage  => {
-	# Valor tomado de la variable PATH_MAX de /usr/src/linux-headers-2.6.28-15/include/linux/limits.h
-	    data_type => 'varchar(4096)',
-	    is_nullable => 1,
-	}
-	);
+__PACKAGE__->add_columns( id      => { data_type         => 'integer',
+				       is_auto_increment => 1 },
+			  name    => { data_type         => 'varchar(64)' },
+			  user_id => { data_type         => 'integer',
+				       is_auto_increment => 1 },
+			  osi_id  => { data_type         => 'integer',
+				      is_auto_increment  => 1 },
+			  ip      => { data_type         => 'varchar(15)' },
+			  storage => { data_type         => 'varchar(4096)',
+					is_nullable      => 1 } );
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint(['name']);
 __PACKAGE__->belongs_to(user => 'QVD::DB::Result::User', 'user_id');
