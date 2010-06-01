10518b0882e4db5abc1443a76d1bb7938b7b69c9
Salvador Fandino <salvador@qindel.com>
2010-06-01 07:55:38 +0000

    code layout

--- ext/QVD-DB/lib/QVD/DB/Result/User.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/User.pm
@@ -3,39 +3,17 @@ use base qw/DBIx::Class/;
 
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('users');
-__PACKAGE__->add_columns(
-	id => {
-	    data_type => 'integer',
-	    is_auto_increment => 1
-	},
-	login =>  {
-	    data_type => 'varchar(64)'
-	},
-	password => {
-	    data_type => 'varchar(64)'
-	},
-	department => {
-	    data_type => 'varchar(64)',
-	    is_nullable => 1,
-	},
-	telephone => {
-	    data_type => 'varchar(64)',
-	    is_nullable => 1,
-	},
-	email => {
-	    data_type => 'varchar(64)',
-	    is_nullable => 1,
-	},
-#	uid => {
-#	    data_type => 'integer'
-#	}
-	);
+__PACKAGE__->add_columns( id         => { data_type         => 'integer',
+					  is_auto_increment => 1 },
+			  login      => { data_type         => 'varchar(64)' },
+			  # FIXME: get passwords out of this table!
+			  password   => { data_type         => 'varchar(64)' } );
+
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint(['login']);
-#__PACKAGE__->add_unique_constraint(['uid']);
 __PACKAGE__->has_one(extra => 'QVD::DB::Result::User_Extra', 'id');
 __PACKAGE__->has_many(vms => 'QVD::DB::Result::VM', 'user_id');
 __PACKAGE__->has_many(properties => 'QVD::DB::Result::User_Property',
-			'user_id', {join_type => 'INNER'});
+		      'user_id', {join_type => 'INNER'});
 
 1;
--- ext/QVD-DB/lib/QVD/DB/Result/User_Extra.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/User_Extra.pm
@@ -3,24 +3,17 @@ use base qw/DBIx::Class/;
 
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('user_extras');
-__PACKAGE__->add_columns(
-	id => {
-	    data_type => 'integer',
-	    is_auto_increment => 1
-	},
-	department => {
-	    data_type => 'varchar(64)',
-	    is_nullable => 1,
-	},
-	telephone => {
-	    data_type => 'varchar(64)',
-	    is_nullable => 1,
-	},
-	email => {
-	    data_type => 'varchar(64)',
-	    is_nullable => 1,
-	},
-	);
+__PACKAGE__->add_columns( id         => { data_type         => 'integer',
+					  # FIXME: do we really need autoincrement for this?
+					  # it should be set in accordance to the table User.
+					  is_auto_increment => 1 },
+			  department => { data_type         => 'varchar(64)',
+					  is_nullable       => 1 },
+			  telephone  => { data_type         => 'varchar(64)',
+					 is_nullable        => 1 },
+			  email      => { data_type         => 'varchar(64)',
+					  is_nullable       => 1 } );
+
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->belongs_to(user => 'QVD::DB::Result::User', 'id');
 
