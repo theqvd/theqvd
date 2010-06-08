380159be3c656610c452db17415201a6e791f96a
Salvador Fandino <salvador@qindel.com>
2010-06-08 09:22:43 +0000

    set db client socket to keepalive

--- ext/QVD-DB/lib/QVD/DB.pm
+++ ext/QVD-DB/lib/QVD/DB.pm
@@ -8,25 +8,51 @@ use strict;
 use Carp;
 use DBIx::Class::Exception;
 
-require QVD::Config;
+use Socket qw(IPPROTO_TCP SOL_SOCKET SO_KEEPALIVE);
+use Socket::Linux qw(TCP_KEEPIDLE TCP_KEEPINTVL TCP_KEEPCNT);
+
+use QVD::Config;
 
 use parent qw(DBIx::Class::Schema);
 
 __PACKAGE__->load_namespaces(result_namespace => 'Result');
 __PACKAGE__->exception_action(sub { croak @_ ; DBIx::Class::Exception::throw(@_);});
 
+
+my $db_name   = core_cfg('database.name');
+my $db_user   = core_cfg('database.user');
+my $db_host   = core_cfg('database.host');
+my $db_passwd = core_cfg('database.password');
+
+my $db_connect_timeout = core_cfg('internal.database.client.connect.timeout');
+my $db_keepidle        = core_cfg('internal.database.client.socket.keepidle');
+my $db_keepintvl       = core_cfg('internal.database.client.socket.keepintvl');
+my $db_keepcnt         = core_cfg('internal.database.client.socket.keepcnt');
+
 sub new {
     my $class = shift;
-    my $name = QVD::Config::core_cfg('database.name');
-    my $user = QVD::Config::core_cfg('database.user');
-    my $host = QVD::Config::core_cfg('database.host');
-    my $passwd = QVD::Config::core_cfg('database.password');
-    $class->SUPER::connect("dbi:Pg:dbname=$name;host=$host",
-			   $user, $passwd,
+    $class->SUPER::connect("dbi:Pg:dbname=$db_name;host=$db_host;connect_timeout=$db_connect_timeout",
+			   $db_user, $db_passwd,
                            { RaiseError => 1,
 			     AutoCommit => 1,
                              quote_char => '"',
-			     name_sep   => '.' });
+			     name_sep   => '.',
+			     on_connect_call => \&_make_pg_socket_keepalive });
+}
+
+sub _make_pg_socket_keepalive {
+    my $storage = shift;
+    my $dbh = $storage->_dbh // die "not connected to database";
+    # we have to duplicate the socket as setsockopt does not accept a
+    # file descriptor
+    open my $socket, '+<&', $dbh->{pg_socket};
+    unless (setsockopt($socket, SOL_SOCKET,  SO_KEEPALIVE,   1) and
+	    # see tcp(7)
+	    setsockopt($socket, IPPROTO_TCP, TCP_KEEPIDLE,  10) and
+	    setsockopt($socket, IPPROTO_TCP, TCP_KEEPINTVL,  5) and
+	    setsockopt($socket, IPPROTO_TCP, TCP_KEEPCNT,    3)) {
+	die "Unable to set database client socket keepalive options: $!";
+    }
 }
 
 my %initial_values = ( VM_State   => [qw(stopped starting running
