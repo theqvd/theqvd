709883a69691d43b38566e3b84ce8eb5bf212e4a
Joni Salonen <jsalonen@qindel.com>
2010-07-27 10:34:47 +0000

    Return a random running unblocked host

--- ext/QVD-L7R/lib/QVD/L7R/LoadBalancer/Plugin/Default.pm
+++ ext/QVD-L7R/lib/QVD/L7R/LoadBalancer/Plugin/Default.pm
@@ -9,8 +9,9 @@ use QVD::Log;
 use parent 'QVD::L7R::LoadBalancer::Plugin';
 
 sub get_free_host {
-    my @hosts = map $_->id, rs(Host)->all;
-    $hosts[rand @hosts];
+    my @hosts = map $_->host_id, rs(Host_Runtime)->search({state   => 'running',
+							   blocked => 'false'});
+    @hosts > 0 ? $hosts[rand @hosts] : undef;
 }
 
 1;
