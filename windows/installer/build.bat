e04539d8b023e5bb1e885c1c487809c5cdb576aa
Hugo Cornejo <hcornejo@qindel.com>
2010-07-30 07:52:52 +0000

    Fixes in build.bat to make the win32 executable work.

--- windows/installer/build.bat
+++ windows/installer/build.bat
@@ -1,28 +1,42 @@
-pp -x -gui -I ^
-..\..\ext\QVD-HTTPC\lib ^
--I ..\..\ext\QVD-URI\lib ^
--a system32\advapi32.dll ^
--a system32\cyggcc_s-1.dll ^
--a system32\cygjpeg-7.dll ^
--a system32\cygpng12.dll ^
--a system32\cygstdc++-6.dll ^
--a system32\cygwin1.dll ^
--a system32\cygXcomp.dll ^
--a system32\cygz.dll ^
--a system32\libeay32_.dll ^
--a system32\libssl32_.dll ^
--a system32\mingwm10.dll ^
--a system32\nxproxy.exe ^
--a C:\strawberry\perl\site\lib\auto\Crypt\OpenSSL\X509\ ^
--I C:\strawberry\perl\site\lib\auto\Crypt\OpenSSL\X509\ ^
--I ..\..\ext\QVD-Config\lib ^
--I ..\..\ext\QVD-Log\lib ^
--I ..\..\ext\QVD-Client\lib ^
--I c:\strawberryperl\perl\site\lib ^
--l C:\strawberry\perl\vendor\lib\Alien\wxWidgets\msw_2_9_0_uni_gcc_3_4\lib\wxbase290u_gcc_custom.dll ^
--l C:\strawberry\perl\vendor\lib\Alien\wxWidgets\msw_2_9_0_uni_gcc_3_4\lib\wxmsw290u_core_gcc_custom.dll ^
--l C:\strawberry\perl\vendor\lib\Alien\wxWidgets\msw_2_9_0_uni_gcc_3_4\lib\wxmsw290u_adv_gcc_custom.dll ^
--I ..\..\ext\QVD-HTTP\lib\ ^
+pp -x -gui ^
 -I ..\..\ext\IO-Socket-Forwarder\lib ^
---icon QVD-Client\pixmaps\qvd.ico -o qvd-client.exe ^
+-I ..\..\ext\QVD-Config\lib ^
+-I ..\..\ext\QVD-Client\lib ^
+-I ..\..\ext\QVD-HTTP\lib\ ^
+-I ..\..\ext\QVD-HTTPC\lib ^
+-I ..\..\ext\QVD-Log\lib ^
+-I ..\..\ext\QVD-URI\lib ^
+-I c:\strawberry\perl\perl\site\lib ^
+-l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxbase28u_gcc_custom.dll ^
+-l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_adv_gcc_custom.dll ^
+-l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_core_gcc_custom.dll ^
+-l C:\strawberry\perl\site\lib\auto\Net\SSLeay\libeay32.dll ^
+-l C:\strawberry\perl\site\lib\auto\Net\SSLeay\ssleay32.dll ^
+-l C:\strawberry\perl\site\lib\auto\Crypt\OpenSSL\X509\X509.dll ^
+--icon QVD-Client\pixmaps\qvd.ico ^
+-o qvd-client.exe ^
 ..\..\ext\QVD-Client\bin\qvd-gui-client.pl
+
+rem -l C:\strawberry\perl\site\lib\auto\Crypt\OpenSSL\X509\libeay32.dll ^
+
+rem -a system32\libeay32_.dll ^
+rem -a system32\libssl32_.dll ^
+rem -a system32\advapi32.dll ^
+rem -a system32\cyggcc_s-1.dll ^
+rem -a system32\cygjpeg-7.dll ^
+rem -a system32\cygpng12.dll ^
+rem -a system32\cygstdc++-6.dll ^
+rem -a system32\cygwin1.dll ^
+rem -a system32\cygXcomp.dll ^
+rem -a system32\cygz.dll ^
+rem -a system32\mingwm10.dll ^
+rem -a system32\nxproxy.exe ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxbase28u_net_gcc_custom.dll ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxbase28u_xml_gcc_custom.dll ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_aui_gcc_custom.dll ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_gl_gcc_custom.dll ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_html_gcc_custom.dll ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_media_gcc_custom.dll ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_richtext_gcc_custom.dll ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_stc_gcc_custom.dll ^
+rem -l C:\strawberry\perl\site\lib\Alien\wxWidgets\msw_2_8_10_uni_gcc_3_4\lib\wxmsw28u_xrc_gcc_custom.dll ^
