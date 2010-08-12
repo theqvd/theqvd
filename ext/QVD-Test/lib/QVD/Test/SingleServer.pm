3cb1a8ae0fa7484bc3d2d240eae63c07ecd89907
Joni Salonen <jsalonen@qindel.com>
2010-08-12 12:25:09 +0000

    Monitor that the node does start

--- ext/QVD-Test/lib/QVD/Test/SingleServer.pm
+++ ext/QVD-Test/lib/QVD/Test/SingleServer.pm
@@ -13,24 +13,34 @@ BEGIN {
 use QVD::Config;
 use QVD::DB::Simple;
 
-my $bridge 	= cfg('vm.network.bridge');
-my $nodename 	= cfg('nodename');
+my $host;
 
 sub check_environment : Test(startup => 6) {
     ok(-f '/etc/qvd/node.conf',		'Existence of QVD configuration, node.conf');
     ok(-f '/usr/bin/qvd-noded.pl',	'QVD node installation');
 
-    ok($bridge,				'Bridge configuration');
+    my $bridge = cfg('vm.network.bridge');
+    ok($bridge,				'Bridge definition in node.conf');
     ok(!system("ip addr show $bridge"), "Existence of VM network bridge $bridge");
 
-    ok($nodename,			'Node name configuration');
-    is(rs(Host)->find({name => $nodename})->count, 1, 
+    my $nodename = cfg('nodename');
+    ok($nodename,			'Node name definition in node.conf');
+    is(rs(Host)->search({name => $nodename})->count, 1, 
 					"Presence of node $nodename in the database");
 
+    $host = rs(Host)->search({name => $nodename})->first;
 }
 
 sub zz_start_node : Test(startup) {
+    my $nodert = $node->runtime;
+    my $orig_ts = $nodert->ok_ts;
+    my $start_timeout = 10;
     system('/usr/bin/qvd-noded.pl start');
+    while ($nodert->ok_ts == $orig_ts) {
+	sleep 1;
+	$nodert->discard_changes;
+	$start_timeout-- or fail("Node didn't start");
+    }
 }
 
 sub aa_stop_node : Test(shutdown) {
