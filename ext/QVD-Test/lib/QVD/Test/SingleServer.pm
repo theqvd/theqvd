4aa3130c4f83870609a5efe50120bbda7884f05c
Joni Salonen <jsalonen@qindel.com>
2010-08-12 10:03:42 +0000

    More environment tests

--- ext/QVD-Test/lib/QVD/Test/SingleServer.pm
+++ ext/QVD-Test/lib/QVD/Test/SingleServer.pm
@@ -5,17 +5,25 @@ use strict;
 use warnings;
 
 use Test::More qw(no_plan);
+
+BEGIN {
+    $QVD::Config::USE_DB = 0;
+}
+
 use QVD::Config;
 use QVD::DB::Simple;
 
 my $bridge 	= cfg('vm.network.bridge');
 my $nodename 	= cfg('nodename');
 
-sub check_environment : Test(startup => 3) {
+sub check_environment : Test(startup => 6) {
     ok(-f '/etc/qvd/node.conf',		'Existence of QVD configuration, node.conf');
     ok(-f '/usr/bin/qvd-noded.pl',	'QVD node installation');
+
+    ok($bridge,				'Bridge configuration');
     ok(!system("ip addr show $bridge"), "Existence of VM network bridge $bridge");
 
+    ok($nodename,			'Node name configuration')
     is(rs(Host)->find({name => $nodename})->count, 1, 
 					"Presence of node $nodename in the database");
 
