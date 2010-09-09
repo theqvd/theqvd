796f57727b17a364cd2b9ca71a546268bcff55e2
Salvador Fandino <salvador@qindel.com>
2010-09-09 09:15:19 +0000

    cleanup

--- ext/QVD-DB/lib/QVD/DB/Result/VM_State.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/VM_State.pm
@@ -3,11 +3,8 @@ use base qw/DBIx::Class/;
 
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('vm_states');
-__PACKAGE__->add_columns(
-	name => {
-	    data_type => 'varchar(12)'
-	}
-	);
+__PACKAGE__->add_columns( name => { data_type => 'varchar(12)' } );
+
 __PACKAGE__->set_primary_key('name');
 __PACKAGE__->has_many(runtime => 'QVD::DB::Result::VM_Runtime', 'vm_state');
 
