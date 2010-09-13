2df3510d451322fa1f50ced2e5c6fd454ea30f9e
Salvador Fandino <salvador@qindel.com>
2010-09-13 11:55:36 +0000

    create /var/run/qvd on startup, solves #542

--- ext/QVD-Node/bin/qvd-noded.pl
+++ ext/QVD-Node/bin/qvd-noded.pl
@@ -8,6 +8,14 @@ use QVD::Node;
 use QVD::Config;
 use QVD::Log;
 
+my $path_run = core_cfg('path.run');
+mkdir $path_run, 0700;
+
+unless (-d $path_run) {
+    ERROR "Unable to create directory $path_run";
+    exit (1);
+}
+
 $App::Daemon::pidfile = core_cfg('noded.pid_file');
 $App::Daemon::as_user = core_cfg('noded.as_user');
 
