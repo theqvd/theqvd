4f56323495da5b641e01022f0b051c1999915551
Nicolas Arenas <narenas@qindel.com>
2010-10-14 11:36:10 +0000

    vmkiller working

--- ext/QVD-VMKiller/lib/QVD/VMKiller.pm
+++ ext/QVD-VMKiller/lib/QVD/VMKiller.pm
@@ -11,8 +11,10 @@ use Proc::ProcessTable;
 sub kill_dangling_vms {
     my $t = Proc::ProcessTable->new;
     foreach my $p (@{$t->table}) {
-        if ($p->cmndline =~ /^kvm/) {
-            say $p->cmndline
+        if ($p->ppid == 1 and $p->cmndline =~ m|^kvm.*\s-name\s+qvd/(\d+)/|) {
+            my $pid = $p->pid;
+            warn "killing dangling VM $1, pid: $pid\n";
+            kill KILL => $pid;
         }
     }
 }
