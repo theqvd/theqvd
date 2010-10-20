b77b511ed0d9ad2eaee4c95fcdb7973cb8188620
Joni Salonen <jsalonen@qindel.com>
2010-10-20 15:30:26 +0000

    Added abstract + version information

--- ext/QVD-L7R-LoadBalancer/lib/QVD/L7R/LoadBalancer.pm
+++ ext/QVD-L7R-LoadBalancer/lib/QVD/L7R/LoadBalancer.pm
@@ -38,3 +38,28 @@ sub get_free_host {
 sub params { %{shift->{params}} }
 
 1;
+__END__
+
+=head1 NAME
+
+QVD::L7R::LoadBalancer - Load balancer component for QVD
+
+=head1 SYNOPSIS
+
+    my $lb = new QVD::L7R::LoadBalancer();
+    my $host_id = $lb->get_free_host;
+    
+=head1 DESCRIPTION
+
+The QVD load balancer component is used to find a free host for starting
+virtual machines on the QVD cluster. It uses a plugin mechanism; the plugins
+are subclasses of C<QVD::L7R::LoadBalancer>. Which plugin is used is determined
+by the C<l7r.loadbalancer.plugin> configuration setting.
+
+=head1 COPYRIGHT
+
+Copyright 2009-2010 by Qindel Formacion y Servicios S.L.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU GPL version 3 as published by the Free
+Software Foundation.
