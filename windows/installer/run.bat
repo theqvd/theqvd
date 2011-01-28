31d7061e60914e1b1ffa081a1b797b0de34fc4b1
Joni Salonen <jsalonen@qindel.com>
2011-01-28 11:20:27 +0000

    Helper batch script for client test runs before compiling.

---
+++ windows/installer/run.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set QVDPATH=.
+
+perl ^
+-I ..\..\ext\IO-Socket-Forwarder\lib ^
+-I ..\..\ext\QVD-Config\lib ^
+-I ..\..\ext\QVD-Client\lib ^
+-I ..\..\ext\QVD-HTTP\lib\ ^
+-I ..\..\ext\QVD-HTTPC\lib ^
+-I ..\..\ext\QVD-Log\lib ^
+-I ..\..\ext\QVD-URI\lib ^
+..\..\ext\QVD-Client\bin\qvd-gui-client.pl
