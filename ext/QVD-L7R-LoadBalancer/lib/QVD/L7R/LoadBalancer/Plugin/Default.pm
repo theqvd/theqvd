theqvd/theqvd

commit 2ceded3b4bb263c0ee8390187717ad68040a34b1

change vm load balancing algorithm
git-svn-id: https://svn.qindel.com/repos/main/projects/qindel/QVD/trunk@10140 4046b530-fbad-4e16-af16-ee11f0b55167

ext/QVD-L7R-LoadBalancer/lib/QVD/L7R/LoadBalancer/Plugin/Default.pm
@@ -2,6 +2,7 @@ package QVD::L7R::LoadBalancer::Plugin::Default;
 
 use strict;
 use warnings;
+use Carp;
 
 use QVD::DB::Simple;
 use QVD::Config;
@@ -9,21 +10,55 @@ use QVD::Log;
 
 use parent 'QVD::L7R::LoadBalancer::Plugin';
 
-my $ram	   = cfg('l7r.loadbalancer.plugin.default.weight.ram');
-my $cpu	   = cfg('l7r.loadbalancer.plugin.default.weight.cpu');
-my $random = cfg('l7r.loadbalancer.plugin.default.weight.random');
+my $weight_ram	  = cfg('l7r.loadbalancer.plugin.default.weight.ram',   1000);
+my $weight_cpu	  = cfg('l7r.loadbalancer.plugin.default.weight.cpu',   1000);
+my $weight_random = cfg('l7r.loadbalancer.plugin.default.weight.random', 100);
 
 sub get_free_host {
     my $conditions = { backend => 'true',
 		       blocked => 'false',
 		       state   => 'running' };
 
-    my $attr = { columns  => 'host_id', 
-		 join     => 'host',
-		 order_by => \"$ram*usable_ram+$cpu*usable_cpu+$random*1000*random() DESC"};
+    my %host;
 
-    my $hrt = rs(Host_Runtime)->search($conditions, $attr)->first;
-    return $hrt->host_id;
+    for my $hrt (rs(Host_Runtime)->search({ backend => 'true',
+                                            blocked => 'false',
+                                            state => 'running' })) {
+        my $id = $hrt->host_id;
+        $host{$id} =  { ram => $hrt->usable_ram,
+                        cpu => $hrt->usable_cpu };
+    }
+
+    for my $vms (rs(VM_Runtime)->search(undef,
+                                        { select   => ['host_id', { count => 'vm_id'}],
+                                          as       => ['host_id', 'vm_count'],
+                                          group_by => ['host_id'] })) {
+        my $id = $queued->host_id;
+        $host{$id}{vms} = $queued->get_column('vm_count') if defined $id;
+
+    }
+
+    my $best;
+    for my $id (keys %host) {
+        my $host = $host{$id};
+        my $vms = ++($host->{vms});
+        my $cap = $weight_ram * $host->{ram} / $vms + $weight_cpu * $host->{cpu} / $vms + rand $weight_random;
+        $host->{cap} = $cap;
+        if (!defined $best or $host{$best}{cap} < $cap) {
+            $best = $id;
+        }
+    }
+
+    # TODO: allow to set limits for the number of virtual machines
+    # running on any host and keep (reintroduce) realtime usage of RAM
+    # and CPU
+
+    return $best if defined $best;
+
+
+    my $msg = "Unable to assign vm to host, there is no host available";
+    ERROR $msg;
+    croak $msg;
 }
 
 1;

ext/QVD-HKD/lib/QVD/HKD.pm
@@ -830,14 +830,11 @@ sub _update_load_balancing_data {
     # TODO: move this code into an external module!
     my $meminfo_lines = slurp('/proc/meminfo', array_ref => 1);
     my %meminfo = map { /^([^:]+):\s*(\d+)/; $1 => $2 } @$meminfo_lines;
-    my $usable_ram = min($meminfo{MemFree}, $meminfo{MemTotal}-2*1024*1024);
 
     my $num_vms = $hkd->{host_runtime}->vms->count;
 
-    $hkd->{host_runtime}->update({
-	    usable_cpu => $bogomips - $bogomips_per_vm*$num_vms,
-	    usable_ram => $usable_ram/1000,
-	});
+    $hkd->{host_runtime}->update({ usable_cpu => $bogomips,
+                                   usable_ram => $meminfo{MemTotal}/1000 });
 }
 
 sub _check_l7rs {

commit 525190ba1a87421a0151dc678f499a4759f01387

Fix for a bug introduced in r10140 that went unnoticed since (!).

ext/QVD-L7R-LoadBalancer/lib/QVD/L7R/LoadBalancer/Plugin/Default.pm
@@ -17,9 +17,10 @@ my $weight_random = cfg('l7r.loadbalancer.plugin.default.weight.random', 100);
 sub get_free_host {
     my %host;
 
-    for my $hrt (rs(Host_Runtime)->search({ backend => 'true',
+    for my $hrt (rs(Host_Runtime)->search({ 'host.backend' => 'true',
                                             blocked => 'false',
-                                            state => 'running' })) {
+                                            state => 'running' },
+                                            { join => 'host' })) {
         my $id = $hrt->host_id;
         $host{$id} =  { ram => $hrt->usable_ram,
                         cpu => $hrt->usable_cpu };