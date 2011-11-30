49871632f3a6c2672a0148b1fb8e796eca3e2ed2
Nicolas Arenas <narenas@qindel.com>
2011-11-30 18:20:53 +0000

    removing QVD-Node as no longer needed

---
+++ ext/QVD-Node-old/bin/qvd-noded.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use App::Daemon qw/daemonize/;
+use QVD::Node;
+use QVD::Config;
+use QVD::Log;
+
+my $path_run = core_cfg('path.run');
+mkdir $path_run, 0755;
+
+unless (-d $path_run) {
+    ERROR "Unable to create directory $path_run";
+    exit (1);
+}
+
+$App::Daemon::pidfile = core_cfg('noded.pid_file');
+$App::Daemon::as_user = core_cfg('noded.as_user');
+
+daemonize;
+my $ok = QVD::Node->new->run;
+exit ($ok ? 0 : 1);
+
+__END__
