6e533dc272b27f1a0c0a68dc8446f7dd9097dee9
Salvador Fandino <salvador@qindel.com>
2011-12-01 11:24:43 +0000

    fix several references to an unexistant $host variable

--- ext/QVD-L7R-LoadBalancer/lib/QVD/L7R/LoadBalancer/Plugin/Default.pm
+++ ext/QVD-L7R-LoadBalancer/lib/QVD/L7R/LoadBalancer/Plugin/Default.pm
@@ -36,8 +36,8 @@ sub get_free_host {
 
     for my $id (keys %host) {
         $host{$id}{cap} =
-            $weight_ram * $host->{ram} / ($host->{vms} + 1) +
-            $weight_cpu * $host->{cpu} / ($host->{vms} + 1) +
+            $weight_ram * $host{ram} / ($host{vms} + 1) +
+            $weight_cpu * $host{cpu} / ($host{vms} + 1) +
             rand $weight_random;
     }
 
@@ -51,7 +51,7 @@ sub get_free_host {
     # and CPU
 
     if (defined $best) {
-        $host->{$best}{vms}++;
+        $host{$best}{vms}++;
         return $best;
     }
 
