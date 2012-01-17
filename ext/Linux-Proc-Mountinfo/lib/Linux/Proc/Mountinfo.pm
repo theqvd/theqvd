cce90b52ed0d9aac7eb3f2dfdfb36812f96b8b67
Salvador Fandino <salvador@qindel.com>
2012-01-17 18:20:42 +0000

    working on parsing /proc/PID/mountinfo correctly

--- ext/Linux-Proc-Mountinfo/lib/Linux/Proc/Mountinfo.pm
+++ ext/Linux-Proc-Mountinfo/lib/Linux/Proc/Mountinfo.pm
@@ -6,6 +6,14 @@ use strict;
 use warnings;
 use Carp;
 
+sub _key {
+    my $mnt = shift;
+    $mnt =~ s|/+|/|g;
+    $mnt =~ s|/?$|/|;
+    $mnt =~ s|([\0/])|\0$1|g;
+    $mnt;
+}
+
 sub read {
     my ($class, %opts) = @_;
     my $mnt = delete $opts{mnt};
@@ -22,6 +30,7 @@ sub read {
     open my $fh, '<', $file or croak "Unable to open '$file': $!";
 
     my @entries;
+    my %entry_by_id;
  OUT: while (<$fh>) {
         chomp;
         my @fields = split;
@@ -38,13 +47,38 @@ sub read {
 
         s/\\([0-7]{1,3})/chr oct $1/g for @fields;
 
-        my @entry = ( @fields[0 .. 5, $dash_ix+1 .. $#fields],
+        my $entry = [ @fields[0 .. 5, $dash_ix+1 .. $#fields],
                       [ @fields[6 .. $dash_ix-1] ],
-                      $., $_ );
-        push @entries, bless \@entry, 'Linux::Proc::Mountinfo::Entry';
+                      $., $_,
+                      _key($fields[4]),
+                      [] ];
+        push @entries, bless $entry, 'Linux::Proc::Mountinfo::Entry';
+        $entry_by_id{$entry->[0]} = $entry;
+    }
+
+    for my $entry (@entries) {
+        my $parent_id = $entry->[1];
+        next if $entry->[0] == $parent_id;
+        my $parent = $entry_by_id{$parent_id} or next;
+        push @{$parent->[13]}, $entry;
     }
 
     bless \@entries, $class;
+}
+
+*new = \&read;
+
+sub at {
+    my ($self, $at) = @_;
+    my $key = _key $at;
+    $_->[12] eq $at and return $_ for @$self;
+    ()
+}
+
+sub root {
+    my $self = shift;
+    $_->[4] eq '/' and return $_ for @$self;
+    ()
 }
 
 package Linux::Proc::Mountinfo::Entry;
@@ -64,15 +98,24 @@ sub optional_fields { [@{shift->[9]}] }
 sub line_number     { shift->[10] }
 sub line            { shift->[11] }
 
+sub _key            { shift->[12] }
+
 sub major           { (split /:/, shift->[2])[0] }
 sub minor           { (split /:/, shift->[2])[1] }
 
+sub children        { bless [@{shift->[13]}], 'Linux::Proc::Mountinfo' }
 
-
-
-
-
-
+sub flatten {
+    my $self = shift;
+    my @flatten;
+    my @queue = $self;
+    while (@queue) {
+        my $first = shift @queue;
+        push @flatten, $first;
+        push @queue, @{$first->[13]}
+    }
+    bless \@flatten, 'Linux::Proc::Mountinfo'
+}
 
 1;
 
@@ -80,24 +123,165 @@ __END__
 
 =head1 NAME
 
-Linux::Proc::Mountinfo - Parse Linux /proc/$pid/mountinfo data
+Linux::Proc::Mountinfo - Parse Linux /proc/$PID/mountinfo data
 
 =head1 SYNOPSIS
 
   use Linux::Proc::Mountinfo;
-  blah blah blah
+
+  my $mi = Linux::Proc::Mountinfo->read;
+
+  my $root = $mi->root;
+  say $root->mount_source, " mounted at /, filesystem type is ", $root->fs_type;
+
+  my $flatten = $root->flatten;
+
+  # umount all file systems but / in an ordered fashion:
+  for (reverse @flatten) {
+    my $mount_point = $_->mount_point;
+    system umount => $mount_point unless $mount_point eq '/';
+  }
 
 =head1 DESCRIPTION
 
-Stub documentation for Linux::Proc::Mountinfo, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+Linux::Proc::Mounts parses the information about mount points provided by
+the Linux kernel at C</proc/$PID/mountinfo>.
 
-Blah blah blah.
+=head1 API
+
+=head2 The Linux::Proc::Mountinfo class
+
+=head3 Internal public representation
+
+The internal representation of the class is an array whose entries can
+be accessed directly unreferencing it. For instance:
+
+  my $mnts = Linux::Proc::Mount->read;
+
+  for my $e (@$mnts) {
+    say $e->spec . " is mounted at " . $e->file . " as " . $e->fstype;
+  }
 
 
-Documentation from Linux C<Documentation/Linux/proc.txt> (available at
-L<http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=blob_plain;f=Documentation/filesystems/proc.txt>).
+=head3 Methods
+
+The following methods are available from this class:
+
+=over 4
+
+=item $mnts = Linux::Proc::Mountinfo->read(%opts)
+
+Reads C</proc/$PID/mountinfo> and returns a new object with the parsed
+data.
+
+The accepted options are as follows:
+
+=over 4
+
+=item mnt => $proc
+
+Overrides the default mount point for the procfs at C</proc>.
+
+=item pid => $pid
+
+Reads the C<mountinfo> file of the process with the given PID. By
+default the C<mouninfo> file of the current process is read.
+
+For instance, for reading C<init> C<mountinfo> file:
+
+  my $mi = Linux::Proc::Mountinfo->read(pid => 1);
+
+=item file => $filename
+
+Reads and parses the file of the given name.
+
+=back
+
+=item $mi->at($path)
+
+Returns an object representing the mount point at the given place.
+
+For instance:
+
+  my $var = $mi->at('/var');
+  print $var->mount_source, " is mounted at /var" if $var;
+
+Returns undef if no file system is mounted there.
+
+=item $mi->root
+
+Returns an object representing the root file system mount point.
+
+=back
+
+=head2 The Linux::Proc::Mountinfo::Entry class
+
+This class is used to represent the single entries on the C<mountinfo>
+file.
+
+=head3 Methods
+
+The methods supported by this class are as follows:
+
+=over 4
+
+=item $mie->mount_id
+
+=item $mie->parent_id
+
+=item $mie->major_minor
+
+=item $mie->root
+
+=item $mie->mount_point
+
+=item $mie->mount_options
+
+=item $mie->fs_type
+
+=item $mie->mount_source
+
+=item $mie->super_options
+
+=item $mie->optional_fields
+
+See the excerpt from the Linux documentation below for the meaning of
+the return values from these accessors.
+
+=item $mie->line_number
+
+Returns the line number of this entry inside the C<mountinfo> file.
+
+=item $mie->line
+
+Returns the full line on the C<mountinfo> file.
+
+=item $mie->major
+
+Returns the major part or the major:minor field.
+
+=item $mie->minor
+
+Returns the minor part of the major:minor field.
+
+=item $mie->children
+
+Returns an Linux::Proc::Mountinfo object containing the mount point
+that are on top of the given one.
+
+=item $mie->flatten
+
+This method is similar to children but returns all the descendants
+instead of just the direct ones.
+
+=back
+
+=head1 Excerpt from Linux documentation
+
+What follows is the documentation available from Linux
+C<Documentation/Linux/proc.txt>
+(L<http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=blob_plain;f=Documentation/filesystems/proc.txt>)
+related to C<mountinfo>.
 
     3.5	/proc/<pid>/mountinfo - Information about mounts
     --------------------------------------------------------
