bfb0639f7331fa1916b42b17406236c987f52ded
Rowan Puttergill <rputtergill@qindel.com>
2012-05-16 14:02:51 +0000

    Mindmap and SVG file for Simplified Desktop Management presentation

---
+++ doc/presentations/Marketing/Simplified Desktop Management with QVD.mm
@@ -0,0 +1,248 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668485019" ID="ID_339903673" MODIFIED="1336672549470" STYLE="bubble" TEXT="Simplified Desktop Management with QVD">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668947599" ID="ID_1693766620" MODIFIED="1336672099186" POSITION="right" STYLE="bubble" TEXT="Managing desktop workstations can be a real headache. ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668978535" ID="ID_971576221" MODIFIED="1336672142896" STYLE="bubble" TEXT="Installing operating systems and software...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669011306" ID="ID_239535812" MODIFIED="1336672121361" STYLE="bubble" TEXT="...configuring software...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669047545" ID="ID_534715470" MODIFIED="1336672125320" STYLE="bubble" TEXT="...keeping track of updates and patches...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669068947" ID="ID_1598518868" MODIFIED="1336672129176" STYLE="bubble" TEXT="...adding users...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669089715" ID="ID_272335777" MODIFIED="1336672138003" STYLE="bubble" TEXT="...enforcing security policies...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669116614" ID="ID_619470970" MODIFIED="1336672138003" STYLE="bubble" TEXT="...providing remote access when users are on the road...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669145248" ID="ID_1793750211" MODIFIED="1336672150583" STYLE="bubble" TEXT="The list seems endless!">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669195717" FOLDED="true" ID="ID_827626513" MODIFIED="1336672153694" STYLE="bubble" TEXT="The time and money that it takes to keep your IT infrastructure running properly sometimes doesn&apos;t seem worth it">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669226096" ID="ID_1974965139" MODIFIED="1336671828510" STYLE="bubble" TEXT="But you could make your life much easier by virtualizing all of your desktops using QVD.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668547513" ID="ID_181570811" MODIFIED="1336672091773" POSITION="right" STYLE="bubble" TEXT="Automated Desktop Provisioning">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669886449" ID="ID_738546078" MODIFIED="1336672179218" STYLE="bubble" TEXT="Whenever a new user joins your organization, you have the problem of setting up and configuring a workstation to meet that user&apos;s requirements.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669948221" ID="ID_558159440" MODIFIED="1336671828493" STYLE="bubble" TEXT="QVD makes this very easy.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336669994760" ID="ID_1107875398" MODIFIED="1336672231690" STYLE="bubble" TEXT="Simply add the user to the QVD environment and set a password.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670017368" ID="ID_579365162" MODIFIED="1336672194338" STYLE="bubble" TEXT="As soon as the user logs into a workstation using the provided username and password, a new virtual desktop environment will be created.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670038730" ID="ID_1741226436" MODIFIED="1336672200993" STYLE="bubble" TEXT="All of the software that the user needs access to...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670059865" ID="ID_792770350" MODIFIED="1336672205062" STYLE="bubble" TEXT="all security policies that apply to that user...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670100203" ID="ID_1267662785" MODIFIED="1336672211035" STYLE="bubble" TEXT="and all of the network resources that should be available...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670127489" ID="ID_998466341" MODIFIED="1336672224133" STYLE="bubble" TEXT="...are instantly taken care of.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670185145" ID="ID_885206420" MODIFIED="1336671828471" STYLE="bubble" TEXT="This is called Automated Desktop Provisioning">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670220092" ID="ID_1290276635" MODIFIED="1336672256638" STYLE="bubble" TEXT="...and is handled by QVD based on the desktop images that you have added to the system for each different set of users.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670244562" ID="ID_953272072" MODIFIED="1336672256635" STYLE="bubble" TEXT="QVD automatically creates a virtual machine for the user and loads the appropriate desktop image into the virtual machine">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670337056" ID="ID_1750946389" MODIFIED="1336672256632" STYLE="bubble" TEXT="...so that your administrative tasks are reduced to simply adding the new user to the system.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668883565" ID="ID_230625267" MODIFIED="1336672300079" STYLE="bubble" TEXT="Integrated authentication services">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670421398" ID="ID_106667894" MODIFIED="1336671828421" STYLE="bubble" TEXT="You don&apos;t have to store your users in QVD.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670433166" ID="ID_1019966399" MODIFIED="1336672266114" STYLE="bubble" TEXT="Thanks to QVD&apos;s programmable nature, plugins are available to use external authentication systems such as Active Directory or any other LDAP repository to handle user provisioning and authentication.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670455243" ID="ID_495122447" MODIFIED="1336672279461" STYLE="bubble" TEXT="By using one of the provided plugins, you can continue to make use of your existing authentication resources to start provisioning virtual desktops to your users from day 1.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668731877" ID="ID_592790814" MODIFIED="1336672289796" POSITION="right" STYLE="bubble" TEXT="Virtual Desktop Image Management">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670673584" ID="ID_97480782" MODIFIED="1336672330547" STYLE="bubble" TEXT="A Desktop Image defines the software and desktop environment that will be presented to a group of users.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670702472" ID="ID_558065185" MODIFIED="1336671828370" STYLE="bubble" TEXT="This helps to reduce the amount of disk space required to support many users...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670721052" ID="ID_1414371473" MODIFIED="1336672339349" STYLE="bubble" TEXT="...and minimizes the amount of work required to keep all users up to date and in sync.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670744224" ID="ID_342188565" MODIFIED="1336672307400" STYLE="bubble" TEXT="QVD uses open source tools">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670777312" ID="ID_1265564806" MODIFIED="1336671828367" STYLE="bubble" TEXT="to create and update desktop images">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670811332" ID="ID_1086043947" MODIFIED="1336672352666" STYLE="bubble" TEXT="This means that you are able to create or update an image from any system that has the correct software installed.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670849592" ID="ID_284262101" MODIFIED="1336672358741" STYLE="bubble" TEXT="By creating or updating a single desktop image, you can service hundreds of users in an instant.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670879736" ID="ID_895230070" MODIFIED="1336672312765" STYLE="bubble" TEXT="Web-based Administration">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670907516" ID="ID_1670932180" MODIFIED="1336672370092" STYLE="bubble" TEXT="For administrators who are not confident working on the command-line, QVD includes a Web-based Administration Panel...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670932233" ID="ID_212563582" MODIFIED="1336672383555" STYLE="bubble" TEXT="...making it simple to load and update disk images so that they are ready to use.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670965956" ID="ID_1765197217" MODIFIED="1336671828310" STYLE="bubble" TEXT="Assigning disk images to users is a simple point and click operation.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670985840" ID="ID_1360784427" MODIFIED="1336672410566" STYLE="bubble" TEXT="For more advanced administration there is a fully scriptable Command Line Utility">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671053357" ID="ID_1808009698" MODIFIED="1336671828304" STYLE="bubble" TEXT="...allowing you to automate many of your Image Management tasks to suit your own procedures.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671220353" ID="ID_128736542" MODIFIED="1336672413607" STYLE="bubble" TEXT="Image Versioning, Rollout and Rollback">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671272569" ID="ID_243368113" MODIFIED="1336671828289" STYLE="bubble" TEXT="Disk Images can be tagged with version numbers">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671297008" ID="ID_1298065168" MODIFIED="1336672421949" STYLE="bubble" TEXT="so that updates can be rolled out in stages to limit corporate-wide impact and reduce support overhead.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671319108" ID="ID_112409378" MODIFIED="1336672419482" STYLE="bubble" TEXT="As a result, rollback to a previous desktop image is as simple as selecting the image version to rollback to and clicking on a button to effect the change.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671342007" ID="ID_1498885751" MODIFIED="1336671828252" STYLE="bubble" TEXT="User data remains unaffected during the operation.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668751540" ID="ID_1122080788" MODIFIED="1336672066405" POSITION="right" STYLE="bubble" TEXT="QVD makes Desktop Operations Management easier.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670508992" ID="ID_981004754" MODIFIED="1336671828247" STYLE="bubble" TEXT="Each Virtual Machine supports direct console access either via a terminal or through the applet provided in the Web Administration Panel.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670573399" ID="ID_1456752858" MODIFIED="1336672439219" STYLE="bubble" TEXT="Adding VNC or SSH support to Virtual Machines is a basic operation.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670591358" ID="ID_1109612848" MODIFIED="1336672455773" STYLE="bubble" TEXT="These facilities allow support staff direct access to the virtual machine that a user is accessing at any point in time.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336670610010" ID="ID_370194322" MODIFIED="1336672465409" STYLE="bubble" TEXT="Logs are also stored separately from the disk image used by the virtual machine, allowing staff to review log data retrospectively to gain further insight into any issues that a user may be experiencing.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668772647" ID="ID_1539461270" MODIFIED="1336672074219" POSITION="right" STYLE="bubble" TEXT="Built-in Security">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671181698" ID="ID_1570764880" MODIFIED="1336671828199" STYLE="bubble" TEXT="QVD provides a secure framework for your users to access their desktops.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671392916" ID="ID_696379945" MODIFIED="1336671828195" STYLE="bubble" TEXT="All connections to virtual desktops are protected using certificate-based encryption, so that user authentication and desktop interaction takes place securely.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671373177" ID="ID_938535703" MODIFIED="1336671828180" STYLE="bubble" TEXT="Since desktop images remain read-only, your virtual desktops are automatically protected from malicious rootkit style attacks, keeping your network virus free...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671418191" ID="ID_989818128" MODIFIED="1336671828160" STYLE="bubble" TEXT="and you can be secure in the knowledge that your data stays in the data center where it belongs.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336668790815" ID="ID_850111198" MODIFIED="1336672079949" POSITION="right" STYLE="bubble" TEXT="Remote Access">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671437443" ID="ID_1236890335" MODIFIED="1336671828132" STYLE="bubble" TEXT="Keep your laptops and mobile devices clean with QVD.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671456382" ID="ID_1682102110" MODIFIED="1336672487558" STYLE="bubble" TEXT="You don&apos;t need to install business critical applications on systems that are outside of your network with QVD.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671472357" ID="ID_1475706755" MODIFIED="1336672493741" STYLE="bubble" TEXT="You also don&apos;t need to worry about tricky VPN configurations.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671493581" ID="ID_1287461130" MODIFIED="1336671828094" STYLE="bubble" TEXT="With the QVD client installed on any laptop or mobile device, your road-warriors and executives can access their desktops from anywhere on the Internet.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671514369" ID="ID_422427934" MODIFIED="1336671828063" STYLE="bubble" TEXT="QVD provides strong security to ensure that communications are encrypted and is capable of working in extremely low-bandwidth environments.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671597281" ID="ID_1563208556" MODIFIED="1336672505128" STYLE="bubble" TEXT="Not only does this mean that your users are able to work as safely and seamlessly as if they are in the office...">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671614098" ID="ID_968596780" MODIFIED="1336672512788" STYLE="bubble" TEXT="...but it also means that you have greater control over data and applications without needing access to the devices that are using your infrastructure.">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff6600" CREATED="1336671633979" ID="ID_552335100" MODIFIED="1336672534340" POSITION="right" STYLE="bubble" TEXT="Try QVD today to see how you can simplify the majority of desktop management and access requirements that you face today. ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</map>
