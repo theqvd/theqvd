5ca7a948980e4573898436475266b10b585ac65e
Salvador Fandino <salvador@qindel.com>
2012-10-03 20:41:27 +0000

    modified version of the run batch file that tells the client program to look for support files under C:\Program Files\QVD

---
+++ windows/installer/run1.bat
@@ -0,0 +1,12 @@
+@echo off
+
+perl ^
+-I ..\..\ext\IO-Socket-Forwarder\lib ^
+-I ..\..\ext\QVD-Config-Core\lib ^
+-I ..\..\ext\QVD-Config\lib ^
+-I ..\..\ext\QVD-Client\lib ^
+-I ..\..\ext\QVD-HTTP\lib\ ^
+-I ..\..\ext\QVD-HTTPC\lib ^
+-I ..\..\ext\QVD-Log\lib ^
+-I ..\..\ext\QVD-URI\lib ^
+..\..\ext\QVD-Client\bin\qvd-gui-client.pl path.client.installation="c:\Program Files\QVD" log.level=DEBUG
