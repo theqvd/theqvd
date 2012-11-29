834a73a7512d5835d16c6bae22e1f593dc785ffb
Salvador Fandino <salvador@qindel.com>
2012-11-29 11:30:51 +0000

    set a value for lxc.cgroup.cpu.shares in order to improve cooperation between containers... really?

--- ext/QVD-HKD/lib/QVD/HKD/VMHandler/LXC.pm
+++ ext/QVD-HKD/lib/QVD/HKD/VMHandler/LXC.pm
@@ -717,6 +717,8 @@ lxc.pts=1
 lxc.rootfs=$self->{os_rootfs}
 lxc.mount.entry=$self->{home_fstab}
 lxc.pivotdir=qvd-pivot
+lxc.cgroup.cpu.shares=100
+
 #lxc.cap.drop=sys_module audit_control audit_write linux_immutable mknod net_admin net_raw sys_admin sys_boot sys_resource sys_time
 
 # Deny access to all devices, except...
