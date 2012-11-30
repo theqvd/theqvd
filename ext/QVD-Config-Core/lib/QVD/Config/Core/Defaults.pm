9e7f4e1520544d55c1a535cd1c7a8010dce54d3e
Salvador Fandino <salvador@qindel.com>
2012-11-30 12:28:10 +0000

    s/poll/pool/

--- ext/QVD-Config-Core/lib/QVD/Config/Core/Defaults.pm
+++ ext/QVD-Config-Core/lib/QVD/Config/Core/Defaults.pm
@@ -334,10 +334,10 @@ internal.database.client.socket.keepidle = 20
 internal.database.client.socket.keepintvl = 5
 internal.database.client.socket.keepcnt = 4
 
-internal.database.poll.connection.timeout = 10
-internal.database.poll.size = 2
-internal.database.poll.connection.delay = 10
-internal.database.poll.connection.retries = 18
+internal.database.pool.connection.timeout = 10
+internal.database.pool.size = 2
+internal.database.pool.connection.delay = 10
+internal.database.pool.connection.retries = 18
 
 internal.hkd.killing.vms.timeout = 120
 
