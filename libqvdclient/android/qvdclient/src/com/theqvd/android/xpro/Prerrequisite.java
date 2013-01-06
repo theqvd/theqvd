9c6f79b59ec78c9426b64390e630d33bc6178449
Nito Martinez <nito@qindel.com>
2013-01-06 07:27:19 +0000

    Replace source code  with externals

--- libqvdclient/android/qvdclient/src/com/theqvd/android/xpro/Prerrequisite.java
+++
@@ -1,8 +0,0 @@
-package com.theqvd.android.xpro;
-
-public interface Prerrequisite {
-	public boolean isInstalled();
-	public void install();
-	public String getButtonText();
-	public String getDescriptionText();
-}
