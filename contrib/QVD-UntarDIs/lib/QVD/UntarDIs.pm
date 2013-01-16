7c9be22d324251b02048bfc54dff4bfb5a6eba92
David Serrano <dserrano@qindel.com>
2013-01-16 14:30:36 +0000

    Partial work towards QVD::UntarDIs.

--- contrib/QVD-UntarDIs/lib/QVD/UntarDIs.pm
+++ contrib/QVD-UntarDIs/lib/QVD/UntarDIs.pm
@@ -1,141 +1,199 @@
+#!/usr/lib/qvd/bin/perl
+
 package QVD::UntarDIs;
-
-use 5.006;
+use warnings;
 use strict;
-use warnings FATAL => 'all';
+use File::Spec;
+use Fcntl qw/:flock/;
+use File::Basename;
+use QVD::Config;
 
-=head1 NAME
+sub new {
+    my ($class, %args) = @_;
+    my %attrs;
 
-QVD::UntarDIs - The great new QVD::UntarDIs!
+    $attrs{$_} = delete $args{$_} for qw/debug/;
 
-=head1 VERSION
+    ## unknown params
+    %args and die sprintf 'unrecognized parameters: %s', join ', ', sort keys %args;
 
-Version 0.01
+    ## required params
+    #exists $attrs{$_} or die "need a '$_' parameter" for qw/foo/;
 
-=cut
+    ## default vals
+    $attrs{'debug'} //= 1;
 
-our $VERSION = '0.01';
-
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use QVD::UntarDIs;
-
-    my $foo = QVD::UntarDIs->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
+    return bless \%attrs, $class;
 }
 
-=head2 function2
-
-=cut
-
-sub function2 {
+## ripped from QVD::HKD::VMHandler::LXC
+sub _mkpath {
+    my ($self, $path, $mask) = @_;
+    $mask ||= 0755;
+    my @dirs;
+    my @parts = File::Spec->splitdir(File::Spec->rel2abs($path));
+    while (@parts) {
+        my $dir = File::Spec->join(@parts);
+        if (-d $dir) {
+            -d $_ or mkdir $_, $mask or return for @dirs;
+            return -d $path;
+        }
+        unshift @dirs, $dir;
+        pop @parts;
+    }
+    return;
 }
 
-=head1 AUTHOR
+## ripped from QVD::HKD::VMHandler::LXC
+sub _run {
+	my ($self, @cmd) = @_;
+	my $cmd_str = join(" ", @cmd);
 
-QVD Team, C<< <qvd-devel at theqvd.com> >>
+	$self->{'debug'} and warn "Running command:  $cmd_str\n";
 
-=head1 BUGS
+	my $ret = system(@cmd);
 
-Please report any bugs or feature requests to C<bug-qvd-untardis at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=QVD-UntarDIs>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+	if ( $? == -1 ) {
+		warn "Failed to execute '$cmd_str': $!\n";
+		return undef;
+	} elsif ( $? & 127 ) {
+		warn sprintf("Command '$cmd_str' died with signal %d, %s coredump\n", ($? & 127),  ($? & 128) ? 'with' : 'without');
+		return undef;
+	} elsif ( ($? >> 8) > 0 )  {
+		warn sprintf("Command '$cmd_str' exited with signal %d\n", $? >> 8);
+		return undef;
+	} else {
+		$self->{'debug'} and warn "Command executed successfully\n";
+	}
 
+	return $ret;
+}
 
+sub calc_paths {
+    my ($self, $di_path) = @_;
 
+    my $basefs_parent = cfg 'path.storage.basefs';
+    $basefs_parent =~ s|/*$||;
 
-=head1 SUPPORT
+    my $basefs = "$basefs_parent/$di_path";
+    my $lockfn = "$basefs_parent/lock.$di_path";
+    my $tmp    = "$basefs_parent/untar-$$-" . rand 100000;
+    $tmp++ while -e $tmp;
 
-You can find documentation for this module with the perldoc command.
+    return $basefs, $lockfn, $tmp;
+}
 
-    perldoc QVD::UntarDIs
+sub get_lock {
+    my ($self, $lockfn) = @_;
+    my $lock;
 
+    unless (open $lock, '>>', $lockfn) {
+        warn "Unable to create or open lock file '$lockfn': $!";
+        return;
+    }
+    $self->{'debug'} and warn "lock file is $lockfn\n";
+    my $retries = 1;
+    while ($retries <= 3) {
+        if (flock($lock, Fcntl::LOCK_EX()|Fcntl::LOCK_NB())) {
+            return $lock;
+        } else {
+            if ($! == POSIX::EAGAIN()) {
+                $self->{'debug'} and warn "Waiting for lock '$lockfn'...\n";
+                sleep 5;
+                $retries++;
+                next;
+            }
+            warn "Unable to acquire lock for '$lockfn': $!";
+            return;
+        }
+    }
+    ## unreached
+}
 
-You can also look for information at:
+sub is_untarred {
+    my ($self, $basefs) = @_;
 
-=over 4
+    if (-d $basefs) {
+        $self->{'debug'} and warn "Image '$basefs' already untarred\n";
+        return;
+    }
+    return 1;
+}
 
-=item * RT: CPAN's request tracker (report bugs here)
+sub create_btrfs_subvol {
+    my ($self, $tmp) = @_;
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=QVD-UntarDIs>
+    if ('btrfs' eq cfg 'vm.lxc.unionfs.type') {
+        if ($self->_run (cfg ('command.btrfs'), 'subvolume', 'create', $tmp)) {
+            warn "Unable to create btrfs subvolume at '$tmp'";
+            return;
+        }
+    } else {
+        unless ($self->_mkpath ($tmp)) {
+            warn "Unable to create directory '$tmp': $!";
+            return;
+        }
+    }
+    return 1;
+}
 
-=item * AnnoCPAN: Annotated CPAN documentation
+sub untar_image {
+    my ($self, $image_path, $tmp) = @_;
 
-L<http://annocpan.org/dist/QVD-UntarDIs>
+    $self->{'debug'} and warn "Untarring image to '$tmp'\n";
+    my @cmd = ( cfg ('command.tar'),
+                'x',
+                -f => $image_path,
+                -C => $tmp );
+    push @cmd, '-z' if $image_path =~ /\.(?:tgz|gz)$/;
+    push @cmd, '-j' if $image_path =~ /\.(?:tbz|bz2)$/;
 
-=item * CPAN Ratings
+    return !$self->_run (@cmd);   ## negate return val from system
+}
 
-L<http://cpanratings.perl.org/d/QVD-UntarDIs>
+sub place_image {
+    my ($self, $tmp, $basefs) = @_;
 
-=item * Search CPAN
+    $self->{'debug'} and warn "Renaming '$tmp' to '$basefs'\n";
+    rename $tmp, $basefs or warn "Rename of '$tmp' to '$basefs' failed: $!\n";
+    unless (-d $basefs) {
+        warn "'$basefs' does not exist or is not a directory";
+        return;
+    }
+    return 1;
+}
 
-L<http://search.cpan.org/dist/QVD-UntarDIs/>
+sub release_lock {
+    my ($self, $lock) = @_;
+    if (!flock $lock, LOCK_UN) {
+        warn "unlock failed: $!";
+        return;
+    }
+    return 1;
+}
 
-=back
+sub untar_dis {
+    my ($self, @images) = @_;
 
+    foreach my $image_path (@images) {
+        if (!-f $image_path) {
+            warn "Image '$image_path' does not exist on disk";
+            next;
+        }
 
-=head1 ACKNOWLEDGEMENTS
+        ## we can use 'basename' which doesn't require access to the db. If
+        ## access is needed for other reason, then change this to use dis.path
+        my $di_path = basename $image_path;
 
+        my ($basefs, $lockfn, $tmp) = $self->calc_paths ($di_path);
+        my $lock = $self->get_lock ($lockfn);
+        next unless $lock;
+        next unless $self->is_untarred ($basefs);
+        next unless $self->create_btrfs_subvol ($tmp);
+        next unless $self->untar_image ($image_path, $tmp);
+        next unless $self->place_image ($tmp, $basefs);
+        next unless $self->release_lock ($lock);
+    }
+}
 
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2013 QVD Team.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-=cut
-
-1; # End of QVD::UntarDIs
+1;
