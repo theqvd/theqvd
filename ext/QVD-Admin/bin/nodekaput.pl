6394192deca8cafac01ea5171f1f6836038708cc
Salvador Fandino <salvador@qindel.com>
2013-03-06 15:01:14 +0000

    ugly script to unassign VMs from some host, try to not use it at all please!

---
+++ ext/QVD-Admin/bin/nodekaput.pl
@@ -0,0 +1,38 @@
+#!/var/lib/qvd/bin/perl
+
+use strict;
+use warnings;
+
+use QVD::DB::Simple;
+
+my ($host_id) = @ARGV;
+defined $host_id or die <<EOU;
+Usage:
+  $0 <node_id>
+
+EOU
+
+my $c;
+txn_eval {
+    my @vms = rs(VM_Runtime)->search({host_id => $host_id});
+    $c = @vms;
+    $c or die "There isn't any virtual machine assigned to host $host_id\n";
+
+    print STDERR "This command may corrupt the QVD database.\n".
+                 "Are you sure you want to unassign ".
+                 "$c virtual machines from host $host_id [y/N]? ";
+
+    my $res = <STDIN>;
+    $res =~ /^y(es)?$/i or die "Aborted!\n";
+
+    $_->unassign for @vms;
+
+};
+
+if ($@) {
+    print $@;
+    exit 1;
+}
+else {
+    print "$c virtual machines have been unassigned from host $host_id\n";
+}
