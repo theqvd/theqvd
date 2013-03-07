40f3b7e01bfed1e21a10cfbd435c9d1e60da58ed
Salvador Fandino <salvador@qindel.com>
2013-03-07 09:55:26 +0000

    exit when hkd exits, honor delay option

--- ext/QVD-HKD/bin/stats-grabber.pl
+++ ext/QVD-HKD/bin/stats-grabber.pl
@@ -22,13 +22,15 @@ sub run_cmd {
 }
 
 while (1) {
-    print "=============================================================";
-    kill USR1 => $pid;
+    print "=============================================================\n";
+    kill USR1 => $pid or kill 0 => $pid or last;
     run_cmd 'date';
     run_cmd 'iostat';
     run_cmd 'vmstat';
     run_cmd 'top -bn1';
     run_cmd 'iotop -bn1';
     run_cmd 'cat /tmp/hkd-vm-states';
-    sleep 10;
+    sleep $delay;
 }
+
+print "unable to signal process $pid\n\n";
