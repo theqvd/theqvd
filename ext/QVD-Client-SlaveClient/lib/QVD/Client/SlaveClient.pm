8180a9dc76b47e3eff79f92bc0ce6647ebf62eb6
Nicolas Arenas <narenas@qindel.com>
2013-03-08 14:30:33 +0000

    Working on Windows support

--- ext/QVD-Client-SlaveClient/lib/QVD/Client/SlaveClient.pm
+++ ext/QVD-Client-SlaveClient/lib/QVD/Client/SlaveClient.pm
@@ -1,13 +1,42 @@
 package QVD::Client::SlaveClient;
 
+use File::Spec;
+use QVD::Config::Core qw(core_cfg set_core_cfg);
+
+our ($WINDOWS, $DARWIN, $user_dir, $app_dir, $user_config_filename, $user_certs_dir, $pixmaps_dir);
+
+BEGIN {
+    $WINDOWS = ($^O eq 'MSWin32');
+    $DARWIN = ($^O eq 'darwin');
+
+
+
+    set_core_cfg('client.log.filename', File::Spec->join($user_dir, 'qvd-client.log'))
+        unless defined core_cfg('client.log.filename', 0);
+    $QVD::Log::DAEMON_NAME='client';
+
+    $app_dir = core_cfg('path.client.installation', 0);
+    if (!$app_dir) {
+        my $bin_dir = File::Spec->join((File::Spec->splitpath(File::Spec->rel2abs($0)))[0, 1]);
+        my @dirs = File::Spec->splitdir($bin_dir);
+        $app_dir = File::Spec->catdir( @dirs[0..$#dirs-1] ); 
+    }
+}
+
 use Fcntl qw(F_GETFL F_SETFL O_NONBLOCK);
 use QVD::HTTPC;
-use QVD::Config::Core qw(core_cfg);
 use QVD::HTTP::StatusCodes qw(:status_codes);
 use JSON qw(decode_json);
 use feature 'switch';
 
 my $command_sftp_server = core_cfg('command.sftp-server');
+if ($WINDOWS) {
+    $command_sftp_server = File::Spec->rel2abs(core_cfg('command.windows.sftp-server'), $app_dir);
+    print core_cfg('command.windows.sftp-server'), "\n";
+    print $command_sftp_server, "\n";
+} elsif ($DARWIN) {
+    $command_sftp_server = core_cfg('command.darwin.sftp-server');
+}
 
 sub new {
     my ($class, $target, %opts) = @_;
@@ -38,6 +67,8 @@ sub help_share {
 sub handle_share {
     my ($self, $path) = @_;
 
+    print "Starting $command_sftp_server...\n";
+
     my ($code, $msg, $headers, $data) =
     $self->{httpc}->make_http_request(PUT => '/shares/'.$path,
         headers => ['Connection: Upgrade', 'Upgrade: qvd:sftp/1.0']);
@@ -52,7 +83,7 @@ sub handle_share {
     close $self->{httpc}->{socket};
 
     chdir $path or die "Unable to chdir to $path: $^E";
-    exec $command_sftp_server 
+    exec($command_sftp_server, $command_sftp_server)
         or die "Unable to exec $command_sftp_server: $^E";
 }
 
