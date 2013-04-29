5f07c4f49dc46e572d130e6a2f7fccbd41868473
Nicolas Arenas <narenas@qindel.com>
2013-04-29 14:02:49 +0000

    Unlink temporary file created for passing socket

--- ext/QVD-Client/lib/QVD/Client/SlaveClient/Windows.pm
+++ ext/QVD-Client/lib/QVD/Client/SlaveClient/Windows.pm
@@ -14,31 +14,33 @@ use File::Temp qw(tempfile);
 use QVD::Log;
 
 BEGIN {
-    Win32::API->Import(kernel32 => 'HANDLE WINAPI CreateNamedPipe(
-        LPCTSTR lpName,
-        DWORD dwOpenMode,
-        DWORD dwPipeMode,
-        DWORD nMaxInstances,
-        DWORD nOutBufferSize,
-        DWORD nInBufferSize,
-        DWORD nDefaultTimeOut,
-        LPSTR lpSecurityAttributes
-        )') or die "Unable to import CreatedNamedPipe";
+    ## The original idea was to use named pipes for IPC. They turned out to not be as reliable as hoped,
+	## so we are using temporary files instead. I'll leave the code for future reference though. -Joni
+    #Win32::API->Import(kernel32 => 'HANDLE WINAPI CreateNamedPipe(
+    #    LPCTSTR lpName,
+    #    DWORD dwOpenMode,
+    #    DWORD dwPipeMode,
+    #    DWORD nMaxInstances,
+    #    DWORD nOutBufferSize,
+    #    DWORD nInBufferSize,
+    #    DWORD nDefaultTimeOut,
+    #    LPSTR lpSecurityAttributes
+    #    )') or die "Unable to import CreatedNamedPipe";
+    #
+    #Win32::API->Import(kernel32 => 'BOOL WINAPI ConnectNamedPipe(HANDLE hNamedPipe, LPSTR lpOverlapped)')
+    #    or die "Unable to import ConnectNamedPipe";
 
-    Win32::API->Import(kernel32 => 'BOOL WINAPI ConnectNamedPipe(HANDLE hNamedPipe, LPSTR lpOverlapped)')
-        or die "Unable to import ConnectNamedPipe";
-
-    Win32::API->Import(kernel32 => 'DWORD WINAPI ResumeThread(HANDLE hThread)')
-        or die "Unable to import ResumeThread";
-
+	# Returns the Windows "ANSI code page" (i.e. the character encoding)
     Win32::API->Import(kernel32 => 'UINT GetACP()')
         or die "Unable to import GetACP";		
-		
-    Win32::API->Import(ws2_32 => 'int WSAGetLastError()')
-        or die "Unable to import WSAGetLastError";
 
+		# Duplicates a windows socket for use in another process
     Win32::API->Import(ws2_32 => 'int WSADuplicateSocket(HANDLE s, DWORD dwProcessId, LPSTR lpProtocolInfo)')
             or die "Unable to import WSADuplicateSocket";
+	
+	# Returns the last windows socket error
+    Win32::API->Import(ws2_32 => 'int WSAGetLastError()')
+        or die "Unable to import WSAGetLastError";
 }
 
 my $app_dir = core_cfg('path.client.installation', 0);
@@ -49,12 +51,11 @@ if (!$app_dir) {
 }
 
 my $command_sftp_server = File::Spec->rel2abs(core_cfg('command.windows.sftp-server'), $app_dir);
-#my $command_sftp_server = 'c:\documents and settings\administrador\Mis documentos\openssh-6.0p1\sftp-server.exe';
 
 sub handle_share {
     my ($self, $path) = @_;
 
-	# FIXME: Might not be correct everywhere
+	# FIXME: Might be wrong for some encodings, we should look into this in more detail!
     my $charset = 'cp'.GetACP();
 	
 	INFO 'Making a PUT request to /shares/'.$path;
@@ -79,27 +80,30 @@ sub handle_share {
     #}
 	
 	# Create tempfile
-	my ($fh, $tempfile) = tempfile(DIR=>'c:/');
+	my ($fh, $tempfile) = tempfile(UNLINK => 1);
+	$fh->autoflush(1);
 	
-    # Start child
-    INFO "** Starting $command_sftp_server to serve $path...\n";
-	#my $cmdline = "sftp-server.exe -l INFO -P $pipe_name";
-	my $command_gdb='c:/mingw/bin/gdb.exe';
-	#my $cmdline = "gdb -ex run -w --directory \"c:\\documents and settings\\administrador\\Mis documentos\\openssh-6.0p1\" --args \"$command_sftp_server\" -l DEBUG3 -F $tempfile";
-    my $cmdline = "sftp-server.exe -l DEBUG3 -F $tempfile";
+	my $logfile = $QVD::Client::App::user_dir . '/sftp-server.log';
+	
+    DEBUG "Starting $command_sftp_server to serve $path...\n";
+
+	# To debug under GDB:
+	# my $command_gdb='c:/mingw/bin/gdb.exe';
+	# my $cmdline = "gdb -ex run -w --directory \"c:\\documents and settings\\administrador\\Mis documentos\\openssh-6.0p1\" --args \"$command_sftp_server\" -l DEBUG3 -F $tempfile";
+    my $cmdline = "sftp-server.exe -l ERROR -F \"$tempfile\" -L \"$logfile\"";
+	#my $cmdline = "sftp-server.exe -l ERROR -F \"$tempfile\" -P \"$pipe_name\"";
     my $child;
     Win32::Process::Create($child, 
         $command_sftp_server, 
         $cmdline,
-        1,                      # inherit handles
+        1,              # inherit handles
         NORMAL_PRIORITY_CLASS | CREATE_NO_WINDOW | CREATE_SUSPENDED,       # creation flags
-		#NORMAL_PRIORITY_CLASS | CREATE_NEW_CONSOLE,
-        $path)
-        or die "Unable to start sftp-server: $^E";
+        $path			# current working directory
+	) or die "Unable to start sftp-server: $^E";
 	
     # Duplicate socket
-    INFO "** Duplicating socket...\n";
-    my $lpProtocolInfo = "\0"x372; # Apparently WSAPROTOCOL_INFO is 372 bytes long
+	DEBUG "Duplicating socket to send it to sftp-server";
+    my $lpProtocolInfo = "\0" x 372; # Size of WSAPROTOCOL_INFO
     my $handle = FdGetOsFHandle(fileno($self->{httpc}->{socket}));
     if (WSADuplicateSocket($handle, $child->GetProcessID(), $lpProtocolInfo)) {
         die "Unable to duplicate socket: ".WSAGetLastError();
@@ -114,15 +118,15 @@ sub handle_share {
     #INFO "** Connecting to pipe $pipe_name...\n";
     #ConnectNamedPipe($pipe, undef)
     #    or die "Unable to connect to pipe: $^E";
-
+    #
     ## Send "protocol info" to child
     #INFO "** Sending protocol info to child...\n";
     #my $written;
     #WriteFile($pipe, $lpProtocolInfo, 372, $written, [])
     #    or die "Unable to write to pipe: $^E";
     #INFO "** Wrote $written bytes to the pipe...\n";
-
-    # Wait for child
+    #
+    ## Wait for child
     #INFO "** Waiting for child...\n";
     #my $status = $child->Wait(INFINITE);
     #INFO "** Finished. (exit code $status)\n";
