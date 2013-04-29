89c9e4e2ccf3974f0eca8c3e6cac2a42c99193cd
Salvador Fandino <salvador@qindel.com>
2013-04-29 08:47:14 +0000

    make vnc-connect act just as a forwarder

--- ext/QVD-HTTPC/bin/vnc-connect.pl
+++ ext/QVD-HTTPC/bin/vnc-connect.pl
@@ -2,15 +2,39 @@
 
 use strict;
 use warnings;
+use 5.010;
 
+use App::pnc qw(netcat_socket);
+use QVD::DB::Simple;
+use QVD::Config;
 use QVD::HTTP::StatusCodes qw(:status_codes);
 use QVD::HTTPC;
 
-my $ip = shift;
 
-$^F=3;
+my $vm_id = shift // die <<EOU;
+Usage:
+  $0 <vm_id>
 
-my $httpc = QVD::HTTPC->new("$ip:3030") or die $@;
+EOU
+
+my $vm = rs(VM)->find($vm_id);
+my $ip = $vm->ip;
+my $rt = $vm->vm_runtime;
+given ($rt->vm_state) {
+    when ('stopped') {
+        die "machine $vm_id is in state stopped\n";
+    }
+    when ('running') {
+        warn "connecting to machine $vm_id\n";
+    }
+    default {
+        warn "machine $vm_id is in state $_, trying to stablish VNC connection anyway...\n";
+    }
+}
+
+my $port = cfg('internal.vm.port.vma');
+
+my $httpc = QVD::HTTPC->new("${ip}:$port") or die $@;
 
 $httpc->send_http_request(GET => "/vma/vnc_connect",
                           headers => [ 'Connection: Upgrade',
@@ -22,13 +46,7 @@ while (1) {
         print STDERR "$msg\n";
     }
     elsif ($code == HTTP_SWITCHING_PROTOCOLS) {
-        warn "running ssvncviewer\n";
-        my $socket = $httpc->get_socket;
-        my $fn = fileno($socket);
-        
-
-
-        eval { exec strace => -o => '/tmp/out', ssvncviewer => "fd=$fn" };
+        netcat_socket($httpc->get_socket);
         exit 0;
     }
     else {
