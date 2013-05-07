83aeb6a5a925e9b804c5b97bf80c5858fef7cc31
Joni Salonen <jsalonen@qindel.com>
2013-05-07 15:59:22 +0000

    Removing needless methods

--- ext/QVD-Client/lib/QVD/Client/SlaveClient.pm
+++ ext/QVD-Client/lib/QVD/Client/SlaveClient.pm
@@ -41,29 +41,4 @@ sub new {
     }
 }
 
-sub dispatch {
-    my ($self, $command, $help, @args) = @_;
-    
-    my $method = $self->can($help? "help_$command": "handle_$command");
-    if (defined $method) {
-        $self->$method(@args);
-    } else {
-        $self->handle_usage();
-    }
-}
-
-sub help_share {
-    print "Syntax: share /path/to/folder
-
-    Forwards the specified folder to the virtual machine.\n"
-}
-
-sub handle_share {
-}
-
-sub handle_usage {
-    # FIXME
-    print "** Write usage doc!\n";
-}
-
 1;
