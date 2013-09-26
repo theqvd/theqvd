ab0a615cda3decac8cdbb817919979332a6f1094
Nito Martinez <nito@qindel.com>
2013-09-26 16:35:04 +0000

    Create a specific object in the html file

--- libqvdclient/webplugin/test/qvd.js
+++ libqvdclient/webplugin/test/qvd.js
@@ -12,10 +12,10 @@ function select_vm_and_connect(vmid)
     $("#select-vm").html("Connecting to VM "+vmid);
     $("#stopconnection").html('<div class="stoprow">Click here to stop connection</div>');
     $("#stopconnection").click(function() { $(this).off('click'); 
-					    qvdembed.qvd_end_connection(); 
+					    qvd.qvd_end_connection(); 
 					    $(this).html('Ending connection'); 
 					  });
-    qvdembed.qvd_connect_to_vm(vmid);
+    qvd.qvd_connect_to_vm(vmid);
 }
 
 function after_qvd_list_of_vm(qvd, vmlist)
@@ -39,7 +39,7 @@ function after_qvd_list_of_vm(qvd, vmlist)
 	$("#select-vm").html("Please click on the vm you want to connect to");
     } else
     {
-	$("#select-vm").html("No valid VM was found, revise parameters, are host username and pass corect? Error was:"+qvdembed.qvd_get_last_error());
+	$("#select-vm").html("No valid VM was found, revise parameters, are host username and pass corect? Error was:"+qvd.qvd_get_last_error());
     }
 }
 
@@ -54,35 +54,40 @@ function qvdinvoke(login, password, host, port)
 {
     
     $("#vmlist").html("Obtaining list of VMs");
-    qvdembed.qvd_init(host, port, login, password);
-    qvdembed.qvd_set_unknown_cert_callback(unknown_cert_callback);
-    qvdembed.qvd_set_progress_callback(progress_callback);
-    qvdembed.qvd_list_of_vm(after_qvd_list_of_vm);
+    qvd.qvd_init(host, port, login, password);
+    qvd.qvd_set_unknown_cert_callback(unknown_cert_callback);
+    qvd.qvd_set_progress_callback(progress_callback);
+    qvd.qvd_list_of_vm(after_qvd_list_of_vm);
     
+}
+
+function myinit()
+{
+    console.log("myinit");
+    qvd = document.getElementById("qvdobject");;
+    if (!qvd.qvd_get_version())
+	alert("Error loading qvd plugin");
+    console.log(qvd);
+}
+
+function displayversion()
+{
+    var version = qvd.qvd_get_version();
+    console.log(version);
+    $("#version").append(version);
+    
+    
+    var versiontext = qvd.qvd_get_version_text();
+    console.log(versiontext);
+    $("#versiontext").append(versiontext);
+
 }
 
 function loginaction()
 {
     console.log("login_action");
-    var placeholder = $("#placeholder");
-    qvdembed = document.createElement('embed');
-    qvdembed.setAttribute('id', 'myembed');
-    qvdembed.setAttribute('type', 'application/theqvd');
-    qvdembed.setAttribute('style', 'height: 0; width: 0;');
-    placeholder.append(qvdembed);
-    
-    console.log(qvdembed);
-    
-    var version = qvdembed.qvd_get_version();
-    console.log(version);
-    $("#version").append(version);
-    
-    
-    var versiontext = qvdembed.qvd_get_version_text();
-    console.log(versiontext);
-    $("#versiontext").append(versiontext);
     $("#connectionparams").hide();
-
+    
     console.log("value" + $("#login").val());
     qvdinvoke($("#login").val(), $("#password").val(), $("#host").val(), 8443)
 }
