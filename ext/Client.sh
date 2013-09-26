a9a6d41c647b599551c1af252566997d0f83388b
Vadim Troshchinskiy <vtroshchinskiy@qindel.com>
2013-09-26 12:34:56 +0000

    Undoing accidental changes

--- ext/Client.sh
+++ ext/Client.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 
 DEBUG="${DEBUG:+gdb --args}"
 
-$DEBUG exec /usr/lib/qvd/bin/perl -Mlib::glob=*/lib QVD-Client/bin/qvd-gui-client.pl
+$DEBUG exec perl -Mlib::glob=*/lib QVD-Client/bin/qvd-gui-client.pl
