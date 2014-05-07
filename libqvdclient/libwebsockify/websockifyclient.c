11d7619d62fc8003bb97b93f0f856fb287513d3f
Nito Martinez <nito@qindel.com>
2014-05-07 14:06:07 +0000

    Restore comment

--- libqvdclient/libwebsockify/websockifyclient.c
+++ libqvdclient/libwebsockify/websockifyclient.c
@@ -3,7 +3,7 @@
 
 
 int main(int argc, char *argv[], char *envp[]) {
-  websockify(1, "127.0.0.1", 5800, "127.0.0.1", 5900);
+  websockify(0, "127.0.0.1", 5800, "127.0.0.1", 5900);
 
   return 0;
 }
