61d3483331ffcae2e4128feb003ea69de770f07f
Nito Martinez <nito@qindel.com>
2014-06-27 05:47:16 +0000

    Unify version to 1.1.1

--- libqvdclient/c/qvdclient.h
+++ libqvdclient/c/qvdclient.h
@@ -12,8 +12,8 @@
 /* #define BUFFER_SIZE CURL_MAX_WRITE_SIZE * 2 */
 #define BUFFER_SIZE 65536
 #include "qvdbuffer.h"
-#define QVDVERSION 101
-#define QVDABOUT "Version: 1.1.0. $Id$"
+#define QVDVERSION 111
+#define QVDABOUT "Version: 1.1.1. $Id$"
 #define QVDCHANGELOG "1.1.1 26/06/2014 Upgraded curl to 7.37.0 and nxcomp to 3.5.0.22 and openssl to 1.0.1h. Use implicit curl_global_init\n"\
                      "1.1.0 21/06/2014 Added support for restart -r, and to reconnect twice\n"\
                      "1.0.1 12/10/2013 in the client added support for environment vars QVDLOGIN QVDPASSWORD and QVDHOST\n"\
