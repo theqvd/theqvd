ca375c1a7b2aac7528caf805dd420c6904870307
Benjamin Ramirez <bramirez@qindel.com>
2014-08-01 12:22:35 +0000

    added QVD::DB::Result::Tenant y QVD::DB::Result::Role

---
+++ ext/QVD-DB/lib/QVD/DB/Result/Role.pm
@@ -0,0 +1,17 @@
+package QVD::DB::Result::Role;
+use base qw/DBIx::Class/;
+
+use strict;
+use warnings;
+
+__PACKAGE__->load_components(qw/Core/);
+__PACKAGE__->table('roles');
+__PACKAGE__->add_columns(id          => { data_type => 'integer',
+                                           is_auto_increment => 1 },
+                          name        => { data_type => 'varchar(80)' });
+
+__PACKAGE__->set_primary_key('id');
+__PACKAGE__->add_unique_constraint(['name']);
+__PACKAGE__->has_many(users => 'QVD::DB::Result::User', 'role_id', { cascade_delete => 0 } );
+
+1;
