13b334fa6bf47d46482509fbc78e567be6285b44
Benjamin Ramirez <bramirez@qindel.com>
2014-11-20 15:16:23 +0000

    Fixed problems in config functions

--- ext/QVD-Admin4/bin/wat.pl
+++ ext/QVD-Admin4/bin/wat.pl
@@ -1,12 +1,12 @@
 #!/usr/lib/qvd/bin/perl
 use Mojolicious::Lite;
-use lib::glob '/home/benjamin/wat/*/lib/';
+use lib::glob '/home/qindel/WAT/*/lib/';
 use QVD::Admin4::REST;
 use Mojo::JSON qw(encode_json decode_json);
 use QVD::Admin4::Exception;
 use QVD::Config;
 use MojoX::Session;
-app->config(hypnotoad => {listen => ['http://192.168.56.101:3000']});
+app->config(hypnotoad => {listen => ['http://192.168.3.5:3000']});
 
 my $QVD_ADMIN4_API = QVD::Admin4::REST->new();
 my $DB_CONNECTION_INFO = "dbi:Pg:dbname=".cfg('database.name').";host=".cfg('database.host');
