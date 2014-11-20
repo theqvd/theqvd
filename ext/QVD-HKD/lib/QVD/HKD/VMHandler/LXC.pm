12b37a11f74e365dee3fbe268de807c245c269c3
David Serrano <dserrano@qindel.com>
2014-11-20 09:15:56 +0000

    LXC 1.0 needs an additional line in the lxc.conf.

--- ext/QVD-HKD/lib/QVD/HKD/VMHandler/LXC.pm
+++ ext/QVD-HKD/lib/QVD/HKD/VMHandler/LXC.pm
@@ -340,6 +340,7 @@ sub _create_lxc {
 
     # FIXME: make this template-able or configurable in some way
     print $cfg_fh <<EOC;
+lxc.aa_profile=unconfined
 lxc.autodev=1
 lxc.hook.autodev=$qvd_lxc_autodev
 lxc.utsname=$self->{name}
