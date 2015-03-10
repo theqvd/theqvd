8925c18b850275f0b3650da1510655c317e06074
Salvador Fandino <salvador@qindel.com>
2015-03-10 09:30:33 +0000

    merge [26129] from multitenant-l7r: fix several errors on Default plugin, use the default tenant when one is not supplied by the user

--- ext/QVD-L7R/lib/QVD/L7R/Authenticator/Plugin/Default.pm
+++ ext/QVD-L7R/lib/QVD/L7R/Authenticator/Plugin/Default.pm
@@ -12,7 +12,7 @@ use parent 'QVD::L7R::Authenticator::Plugin';
 
 sub authenticate_basic {
     my ($plugin, $auth, $login, $passwd, $l7r) = @_;
-    my $tenant_id = $self->{tenant_id};
+    my $tenant_id = $auth->{tenant_id};
     DEBUG "authenticating $login, tenant: $auth->{normalized_tenant} ($tenant_id)";
 
     # Reject passwordless login #1209
@@ -21,7 +21,7 @@ sub authenticate_basic {
     my $salt = cfg('l7r.auth.plugin.default.salt');
     my $token = sha256_base64("$salt$passwd");
 
-    my $rs = rs(User)->search({login => $login, password => $token, tenant_id => $tenant});
+    my $rs = rs(User)->search({login => $login, password => $token, tenant_id => $tenant_id});
     return () unless $rs->count > 0;
     DEBUG "authenticated ok";
     $auth->{user_id} = $rs->first->id;
@@ -34,22 +34,23 @@ sub find_tenant {
     my ($plugin, $auth, $login, $l7r) = @_;
     DEBUG "find_tenant $login";
 
+    my ($login1, $tenant);
     my $separators = cfg('l7r.auth.plugin.default.separators');
     if (length $separators) {
         my $re = $re_cache{$separators} // do {
-            my $str = '(?:' . join('|', map quotemeta split //, $separator) . ')';
+            my $str = '(?:' . join('|', map quotemeta split //, $separators) . ')';
             qr/$str/i;
         };
-        if (my ($login1, $tenant) = split $re, $login, 1) {
-            DEBUG "login '$login' split as tenant: '$tenant', login: '$login1'";
-            return ($tenant, $login1);
-        }
+        ($login1, $tenant) = split $re, $login, 1;
     }
-    if (defined (my $tenant = cfg('l7r.auth.plugin.default.tenant', 0))) {
-        DEBUG "using default tenant '$tenant' with login '$login'";
-        return ($tenant, $login);
+
+    $login1 //= $login;
+    $tenant //= cfg('l7r.auth.plugin.default.tenant', 0);
+    unless (defined $tenant) {
+        ERROR "Unable to infer tenant from login '$login' and no default configured";
+        return;
     }
-    ()
+    ($tenant, $login);
 }
 
 1;
