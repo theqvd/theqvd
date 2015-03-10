3596cd90b6ef5b7f9a93dd383c51f2a6708eead4
Salvador Fandino <salvador@qindel.com>
2015-03-10 09:04:32 +0000

    merge [25906] from multitenant-l7r: add support for per-tenant configurations

--- ext/QVD-DB/lib/QVD/DB/Result/Config.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/Config.pm
@@ -3,18 +3,9 @@ use base qw/DBIx::Class/;
 
 __PACKAGE__->load_components(qw/Core/);
 __PACKAGE__->table('configs');
-__PACKAGE__->add_columns(
-	key => {
-	    data_type => 'varchar(64)'
-	},
-	value => {
-	    data_type => 'varchar(4096)'
-	}
-	);
-__PACKAGE__->set_primary_key('key');
+__PACKAGE__->add_columns( tenant_id => { data_type => 'integer' },
+                          key => { data_type => 'varchar(64)' },
+                          value => { data_type => 'varchar(4096)' } );
+__PACKAGE__->set_primary_key(qw(tenant_id key));
 __PACKAGE__->add_unique_constraint([qw(key)]);
-sub get_has_many { qw(); }
-sub get_has_one { qw(); }
-sub get_belongs_to { qw(); }
-
 1;
