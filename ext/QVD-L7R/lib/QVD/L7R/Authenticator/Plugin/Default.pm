ac2a940b366b9fb2f7ad52eee82cc72545d275ab
Salvador Fandino <salvador@qindel.com>
2015-03-10 09:07:07 +0000

    merge [25909] from multitenant-l7r: add support for the find_tenant method

--- ext/QVD-L7R/lib/QVD/L7R/Authenticator/Plugin/Default.pm
+++ ext/QVD-L7R/lib/QVD/L7R/Authenticator/Plugin/Default.pm
@@ -12,7 +12,8 @@ use parent 'QVD::L7R::Authenticator::Plugin';
 
 sub authenticate_basic {
     my ($plugin, $auth, $login, $passwd, $l7r) = @_;
-    DEBUG "authenticating $login";
+    my $tenant_id = $self->{tenant_id};
+    DEBUG "authenticating $login, tenant: $auth->{normalized_tenant} ($tenant_id)";
 
     # Reject passwordless login #1209
     return () if $passwd eq '';
@@ -20,11 +21,35 @@ sub authenticate_basic {
     my $salt = cfg('l7r.auth.plugin.default.salt');
     my $token = sha256_base64("$salt$passwd");
 
-    my $rs = rs(User)->search({login => $login, password => $token});
+    my $rs = rs(User)->search({login => $login, password => $token, tenant_id => $tenant});
     return () unless $rs->count > 0;
     DEBUG "authenticated ok";
     $auth->{user_id} = $rs->first->id;
     1;
+}
+
+my %re_cache;
+
+sub find_tenant {
+    my ($plugin, $auth, $login, $l7r) = @_;
+    DEBUG "find_tenant $login";
+
+    my $separators = cfg('l7r.auth.plugin.default.separators');
+    if (length $separators) {
+        my $re = $re_cache{$separators} // do {
+            my $str = '(?:' . join('|', map quotemeta split //, $separator) . ')';
+            qr/$str/i;
+        };
+        if (my ($login1, $tenant) = split $re, $login, 1) {
+            DEBUG "login '$login' split as tenant: '$tenant', login: '$login1'";
+            return ($tenant, $login1);
+        }
+    }
+    if (defined (my $tenant = cfg('l7r.auth.plugin.default.tenant', 0))) {
+        DEBUG "using default tenant '$tenant' with login '$login'";
+        return ($tenant, $login);
+    }
+    ()
 }
 
 1;
