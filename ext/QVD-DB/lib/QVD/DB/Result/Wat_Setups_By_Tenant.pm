1310ab6755d26e33b4244187954a13bb40d5bf2c
Benjamin Ramirez <bramirez@qindel.com>
2015-03-12 12:31:18 +0000

    Added DBIXC tables Wat_Setups_By_Administrators/Tenants

---
+++ ext/QVD-DB/lib/QVD/DB/Result/Wat_Setups_By_Tenant.pm
@@ -0,0 +1,22 @@
+package QVD::DB::Result::Wat_Setups_By_Tenant;
+use base qw/DBIx::Class/;
+
+use strict;
+use warnings;
+
+__PACKAGE__->load_components(qw/Core/);
+__PACKAGE__->table('wat_setups_by_tenants');
+__PACKAGE__->add_columns( id          => { data_type => 'integer',
+                                           is_auto_increment => 1 },
+			  block      => { data_type         => 'integer',
+                                          default_value     => 0 },
+			  language      => { data_type         => 'varchar(64)',
+                                             default_value => 'EN' },
+                          tenant_id        => { data_type => 'integer' });
+
+__PACKAGE__->set_primary_key('id');
+__PACKAGE__->add_unique_constraint(['tenant_id']);
+__PACKAGE__->belongs_to(tenant => 'QVD::DB::Result::Tenant', 'tenant_id');
+
+1;
+
