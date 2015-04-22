e61e97c1cb8101e608c6b4dc6592eb043c2fb188
Benjamin Ramirez <bramirez@qindel.com>
2015-04-22 13:30:19 +0000

    Fixed problem when instantiating object_id in log entries

--- ext/QVD-Admin4/lib/QVD/Admin4/LogReport.pm
+++ ext/QVD-Admin4/lib/QVD/Admin4/LogReport.pm
@@ -99,6 +99,7 @@ my $DB;
 sub BUILD
 {
     my $self = shift;
+
     $DB = db();
     
     my $localtime = localtime;
@@ -137,9 +138,10 @@ sub set_object_in_log_entry
     my $self = shift;
 
     if (ref($self->object) =~ /^QVD::DB::Result::.+$/)
-    {
+    {  
 	@{$self->{log_entry}}{qw(object_id object_name)} = 
-	    (eval { $self->object->id } // undef, eval { $self->object->name } // undef);
+	    ( eval { $self->object->get_column('id') } , eval { $self->object->name } // undef);
+
     }
     elsif (ref($self->object) eq 'HASH')
     {
