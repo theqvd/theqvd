ec267c8bb914ec9a37310ae83590ff249b0a167a
Sergio Martin Zarzuelo <szarzuelo@qindel.com>
2015-06-03 09:38:06 +0000

    Clean unused code provided from WAT

--- up/lib/bb-views/proto-details.js
+++ up/lib/bb-views/proto-details.js
@@ -1,7 +1,6 @@
 Wat.Views.DetailsView = Wat.Views.MainView.extend({
     elementId: 0,
     detailsContainer: '.bb-details',
-    sideContainer: '.bb-details-side',
     
     viewKind: 'details',
     
@@ -36,13 +35,6 @@ Wat.Views.DetailsView = Wat.Views.MainView.extend({
         this.templates = $.extend({}, this.templates, templates);
     },
     
-    afterRender: function () {
-        // If this view have Side component, render it after render
-        if (this.renderSide) {
-            this.renderSide();
-        }
-    },
-    
     fetchDetails: function (that) {
         var that = that || this;
         that.model.fetch({      
@@ -53,15 +45,6 @@ Wat.Views.DetailsView = Wat.Views.MainView.extend({
     },
     
     eventsDetails: {
-        'click .js-button-edit': 'openEditElementDialog',
-        'click .js-button-unblock': 'applyUnblock' ,
-        'click .js-button-block': 'applyBlock', 
-        'click .js-button-delete': 'askDelete',
-        'click .js-button-default': 'applyDefault',
-        'click .js-button-start-vm': 'startVM',
-        'click .js-button-stop-vm': 'stopVM',
-        'click .js-button-restart-vm': 'restartVM',
-        'click .js-button-disconnect-all-vms': 'applyDisconnectAll'
     },
 
     render: function () {
@@ -98,179 +81,4 @@ Wat.Views.DetailsView = Wat.Views.MainView.extend({
         Wat.T.translateAndShow();
     },
     
-    applyBlock: function () {
-        this.updateModel({blocked: 1}, {id: this.elementId}, this.fetchDetails);
-    },   
-    
-    applyUnblock: function () {
-        this.updateModel({blocked: 0}, {id: this.elementId}, this.fetchDetails);
-    },  
-    
-    askDelete: function () {
-        Wat.I.confirm('dialog/confirm-undone', this.applyDelete, this);
-    },
-        
-    applyDelete: function (that) {
-        that.deleteModel({id: that.elementId}, that.afterDelete, that.model);
-    },
-    
-    afterDelete: function (that) {
-        //Find the last link to rederect to it after deletion
-        var lastLink = '';
-        var crumb = that.breadcrumbs;
-        while (1) {
-            if (crumb.next != undefined) {
-                if (crumb.link != undefined) {
-                    lastLink = crumb.link;
-                }
-                crumb = crumb.next;
-            }
-            else {
-                break;
-            }
-        }
-        
-        window.location = lastLink;
-    },
-    
-    setDetailsFields: function () {
-        // Get Fields from configuration
-        this.detailsFields = Wat.I.getDetailsFields(this.qvdObj);
-        
-        // Check acls on fields to remove forbidden ones
-        Wat.C.purgeConfigData(this.detailsFields);
-
-        // The superadmin have an extra field on lists: tenant
-        
-        // Every element but the hosts has tenant
-        if (Wat.C.isSuperadmin() && this.qvdObj != 'host') {
-            this.detailsFields.tenant = {
-                'text': 'Tenant',
-                'display': true,
-                'noTranslatable': true
-            };
-        }
-    },
-    
-    // Check some acls to show or not the side of a details view
-    // If no acl pass, return false, otherwise return an object with acls and true or false if pass or not
-    checkSide: function (acls) {
-        var nAcls = acls.length;
-        var pass = 0;
-        var result = {};
-        
-        $.each(acls, function (acl, layer) {
-            if (Wat.C.checkACL(acl)) {
-                pass++;
-                result[acl] = true;
-            }
-            else {
-                result[acl] = false;
-                $(layer).hide();
-            }
-        });
-        
-        if (!pass) {
-            $('.js-details-side').hide();
-            $('.js-details-block').addClass('col-width-100');
-            return false;
-        }
-        //$('.js-details-side').show();
-        //$('.js-details-block').removeClass('col-width-100');
-        return result;
-    },
-    
-    // Return the params to render a embeded side list with object log registers
-    getSideLogParams: function (sideContainer) {
-        var params = {};
-        params.whatRender = 'list';
-        params.listContainer = sideContainer;
-        params.forceListColumns = {
-            see_details: true, 
-            action: true, 
-            datetime: true
-        };
-
-        params.forceSelectedActions = {};
-        params.block = 5;
-        params.filters = {
-            qvd_object: this.qvdObj, 
-            object_id: this.elementId
-        };
-        
-        return params;
-    },
-    
-    renderLogGraph: function (params) {
-        var fields = ["time"];
-        var filters = params.filters;
-        var orderBy = {
-            "field": "id",
-            "order": "-desc"
-        };
-        
-        var that = this;
-        
-        Wat.A.performAction ('log_get_list', {}, filters, {}, function(result){
-            var dataGroups = 50;
-            
-            if (result.retrievedData.total > 0) {
-                var rows = result.retrievedData.rows;
-                
-                var serverTimestamp = (Date.parse(Date()) / 1000);
-                var olderTimestamp = Date.parse(rows[rows.length-1].time) / 1000;
-                var newerTimestamp = Date.parse(rows[0].time) / 1000;
-                
-                // Give it 20% margin
-                var timeFromOlder = parseInt((serverTimestamp - olderTimestamp) * 1);
-                
-                var step = parseInt(timeFromOlder / dataGroups);
-                
-                var graphData = [];
-                for (iMin=olderTimestamp-1; iMin<=serverTimestamp-step; iMin+=step) {
-                    var iMax = (iMin+step)<=serverTimestamp ? iMin+step : serverTimestamp + 1;
-                    
-                    var groupCount = 0;
-                    var groupName = iMin;
-                    
-                    $.each(rows, function (i, v) {
-                        if (!v) {
-                            return;
-                        }
-                        
-                        var stepTimestamp = Date.parse(v.time) / 1000;
-                        if (stepTimestamp > iMin && stepTimestamp <= iMax) {
-                            groupCount++;
-                            delete rows[i];
-                        }
-                    });
-                    
-                    graphData.push({
-                        "id": iMin,
-                        "name": groupName,
-                        "registers": groupCount
-                        }
-                    );
-                }
-                
-                that.loadLogGraphData(graphData);                
-            }
-        }, this, fields, orderBy);
-    },
-    
-    loadLogGraphData: function (data) {
-        if (!data) {
-            return;
-        }
-
-        if ($('#graph-log').html() != undefined) {
-            // Trick to draw bar chart when the div where it will be located will be rendered
-            // We know that it is rendered when CSS width attribute change from 'XXX%' to 'XXXpx'
-            setTimeout(function () {
-                if ($('#graph-log').css('width').indexOf("%") == -1) {
-                    Wat.I.G.drawBarChartLog('graph-log', data);
-                }
-            }, 50);
-        }
-    },
 });
--- up/lib/bb-views/proto-main.js
+++ up/lib/bb-views/proto-main.js
@@ -7,10 +7,6 @@ Wat.Views.MainView = Backbone.View.extend({
     messagetype: 'error',
     dialogConf: {},
     deleteProps: [],
-    deleteACLs: [],
-    addACLs: [],
-    deleteRoles: [],
-    addRoles: [],
     currentMenu: '', // platform-setup
     sideViews: [],
     templates: {},
@@ -49,15 +45,6 @@ Wat.Views.MainView = Backbone.View.extend({
     },
     
     events:  {
-        'input .filter-control>input': 'filterBySubstring'
-    },
-    
-    filterBySubstring: function(e) {
-        // Store typed search to mantain the order in filter task avoiding 
-        // filtering if current typed search doesnt match with stored one
-        Wat.CurrentView.typedSearch = $(e.target).val();
-        
-        this.filter(e);
     },
     
     extendEvents: function (ev) {
@@ -78,214 +65,11 @@ Wat.Views.MainView = Backbone.View.extend({
         return newEvents;
     },
     
-    printBreadcrumbs: function (bc, bcHTML) {
-        // If no bradcrumbs are given, do nothing
-        if ($.isEmptyObject(bc)) {
-            return;
-        }
-        
-        if (bc.link != undefined) {
-            bcHTML += '<a href="' + bc.link + '" data-i18n="' + bc.screen + '"></a>';
-        }
-        else {
-            bcHTML += '<span data-i18n="' + bc.screen + '">' + bc.screen + '</span>';
-        }
-        if (bc.next != undefined) {
-            bcHTML += ' <i class="fa fa-angle-double-right"></i> ';
-            this.printBreadcrumbs (bc.next, bcHTML);
-        }
-        else {
-            bcHTML += '<a href="javascript:" class="fa fa-book js-screen-help screen-help" data-i18n="[title]This section step by step" data-docsection="' + this.qvdObj + '"></a>'
-            $('#breadcrumbs').html(bcHTML);
-        }
-    },
-    
-    // Editor
-    editorElement: function (e) {
-        Wat.I.dialog(this.dialogConf, this);           
-    },
-    
-    fillEditor: function (target, that) {
-        var that = that || Wat.CurrentView;
-        
-        var isSuperadmin = Wat.C.isSuperadmin();
-                
-        if (that.viewKind == 'details' || that.editingFromList) {
-            var editorMode = 'edit';
-            delete that.editingFromList;
-        }
-        else {
-            var editorMode = 'create';
-        }
-        
-        var classifiedByTenant = $.inArray(that.qvdObj, QVD_OBJS_CLASSIFIED_BY_TENANT) != -1;
-        
-        // Get enabled properties value from constant. Properties could be disabled by variable
-        if (that.enabledProperties != undefined) {
-            var enabledProperties =  that.enabledProperties;
-            // Clean enabledProperties variable
-            delete that.enabledProperties;
-        }
-        else {
-            var enabledProperties =  $.inArray(that.qvdObj, QVD_OBJS_WITH_PROPERTIES) != -1;
-        }
-        
-        var enabledCreateProperties = true;
-        var enabledUpdateProperties = true;
-        var enabledDeleteProperties = true;
-
-        if (enabledProperties) {
-            switch (editorMode) {
-                case 'edit':
-                        if (!Wat.C.checkACL(Wat.CurrentView.qvdObj + '.update.properties-create')) {
-                            var enabledCreateProperties = false;
-                        }
-                        if (!Wat.C.checkACL(Wat.CurrentView.qvdObj + '.update.properties-update')) {
-                            var enabledUpdateProperties = false;
-                        }
-                        if (!Wat.C.checkACL(Wat.CurrentView.qvdObj + '.update.properties-delete')) {
-                            var enabledDeleteProperties = false;
-                        }
-                    break;
-                case 'create':
-                        if (!Wat.C.checkACL(Wat.CurrentView.qvdObj + '.create.properties')) {
-                            enabledProperties = false;
-                        }
-                    break;
-            }
-        }
-        
-        // Add common parts of editor to dialog
-        that.template = _.template(
-                    Wat.TPL.editorCommon, {
-                        classifiedByTenant: editorMode == 'create' ? classifiedByTenant : 0,
-                        isSuperadmin: isSuperadmin,
-                        editorMode: editorMode,
-                        blocked: that.model ? that.model.attributes.blocked : 0,
-                        properties: that.model ? that.model.attributes.properties : {},
-                        enabledProperties: enabledProperties,
-                        enabledCreateProperties: enabledCreateProperties,
-                        enabledUpdateProperties: enabledUpdateProperties,
-                        enabledDeleteProperties: enabledDeleteProperties,
-                        cid: that.cid
-                    }
-                );
-        
-        target.html(that.template);
-        
-        if (editorMode == 'create' && isSuperadmin && classifiedByTenant) {
-            
-            var params = {
-                'action': 'tenant_tiny_list',
-                'selectedId': 0,
-                'controlId': 'tenant_editor',
-                'chosenType': 'single100'
-            };
-            
-            Wat.A.fillSelect(params, function () {
-                // Remove supertenant from tenant selector
-                var existsInSupertenant = $.inArray(that.qvdObj, QVD_OBJS_EXIST_IN_SUPERTENANT) != -1;
-
-                if (!existsInSupertenant) {
-                    $('select[name="tenant_id"] option[value="0"]').remove();
-                    
-                    Wat.I.updateChosenControls('[name="tenant_id"]');
-                    $('[name="tenant_id"]').trigger('change');
-                }
-            });
-        }
-        
-        // Add specific parts of editor to dialog
-        that.template = _.template(
-                    that.templateEditor, {
-                        model: that.model || that.collection.where({id: that.selectedItems[0]})[0]
-                    }
-                );
-
-        $(that.editorContainer).html(that.template);
-    },
-    
-    updateElement: function () {
-        this.parseProperties();
-        
+    updateElement: function () {        
         var context = '.editor-container.' + this.cid;
         
         return Wat.I.validateForm(context);
     },  
-    
-    createElement: function () {
-        this.parseProperties();
-        
-        var context = '.editor-container.' + this.cid;
-        
-        return Wat.I.validateForm(context);
-    },
-    
-    // Parse properties from create/edit forms
-    parseProperties: function () {
-        var propNames = $('.' + this.cid + '.editor-container input.custom-prop-name');
-        var propValues = $('.' + this.cid + '.editor-container input.custom-prop-value');
-        
-        var deletedProps = [];
-        var setProps = {};
-        
-        switch(this.viewKind) {
-            case 'list':
-                var createPropertiesACL = this.qvdObj + '.update-massive.properties-create';
-                var updatePropertiesACL = this.qvdObj + '.update-massive.properties-update';
-                var deletePropertiesACL = this.qvdObj + '.update-massive.properties-delete';
-                break;
-            case 'details':
-                var createPropertiesACL = this.qvdObj + '.update.properties-create';
-                var updatePropertiesACL = this.qvdObj + '.update.properties-update';
-                var deletePropertiesACL = this.qvdObj + '.update.properties-delete';
-                break;    
-        }
-        
-        for(i=0;i<propNames.length;i++) {
-            var name = propNames.eq(i);
-            var value = propValues.eq(i);
-            
-            if (!name.val()) {
-                continue;
-            }
-                        
-            // If the element has not data-current attribute means that it's new
-            // New properties with empty name will be ignored
-            if (name.val() !== '' && value.attr('data-current') === undefined && Wat.C.checkACL(createPropertiesACL)) {
-                setProps[name.val()] = value.val();
-            }
-            else {
-                // If the value is different of the data-current attribute means that it's different
-                if (value.attr('data-current') != value.val() && Wat.C.checkACL(updatePropertiesACL)) {
-                    setProps[name.val()] = value.val();
-                }
-            }
-        }
-        
-        if (!Wat.C.checkACL(deletePropertiesACL)) {
-            this.deleteProps = [];
-        }
-        
-        this.properties = {
-            'set' : setProps, 
-            'delete': this.deleteProps
-        };
-        
-        // Restore deleteProps array
-        this.deleteProps = [];
-    },
-    
-    createModel: function (arguments, successCallback) {
-        this.model.setOperation('create');
-        
-        var messages = {
-            'success': 'Successfully created',
-            'error': 'Error creating'
-        };
-        
-        this.saveModel(arguments, {}, messages, successCallback);        
-    },
     
     updateModel: function (arguments, filters, successCallback, model) {
         // If not model is passed, use this.model
@@ -304,20 +88,6 @@ Wat.Views.MainView = Backbone.View.extend({
         };
         
         this.saveModel(arguments, filters, messages, successCallback, model);
-    },
-    
-    deleteModel: function (filters, successCallback, model) {
-        // If not model is passed, use this.model
-        var model = model || this.model;
-        
-        model.setOperation('delete');
-        
-        var messages = {
-            'success': 'Successfully deleted',
-            'error': 'Error deleting'
-        };
-        
-        this.saveModel({}, filters, messages, successCallback, model);
     },
     
     saveModel: function (arguments, filters, messages, successCallback, model) {
@@ -364,29 +134,6 @@ Wat.Views.MainView = Backbone.View.extend({
         });
     },
     
-    openEditElementDialog: function (e) {
-        var that = this;
-        
-        this.templateEditor = Wat.TPL['editor_' + that.qvdObj];
-        
-        this.dialogConf.buttons = {
-            Cancel: function () {
-                $(this).dialog('close');
-            },
-            Update: function () {
-                that.dialog = $(this);
-                that.updateElement();
-            }
-        };
-        
-        this.dialogConf.button1Class = 'fa fa-ban';
-        this.dialogConf.button2Class = 'fa fa-save';
-        
-        this.dialogConf.fillCallback = this.fillEditor;
-        
-        this.editorElement (e);
-    },
-    
     stopVM: function (filters, messages) {        
         var messages = messages || {
             'success': 'Stop request successfully performed',
@@ -404,39 +151,4 @@ Wat.Views.MainView = Backbone.View.extend({
         
         Wat.A.performAction ('vm_user_disconnect', {}, filters, messages, this.fetchList, this);
     },
-    
-    renderRelatedDocs: function () {
-        // The related doc links only will be render for not embeded views
-        if (Wat.CurrentView.qvdObj != this.qvdObj) {
-            return;
-        }
-        
-        var sectionDoc = [];
-        sectionDoc[this.qvdObj] = "This section step by step";
-        this.relatedDoc = $.extend({}, sectionDoc, this.relatedDoc);
-        
-        if (this.relatedDoc) {
-            var that = this;
-            
-            that.template = _.template(
-                    Wat.TPL.relatedDoc, {
-                        relatedDoc: that.relatedDoc,
-                    }
-                );
-
-            $('.bb-related-docs').html(that.template);
-        }
-    },
-    
-    // Fetch details or list depending on the current view kind
-    fetchAny: function (that) {
-        switch (that.viewKind) {
-            case 'list':
-                that.fetchList();
-                break;
-            case 'details':
-                that.fetchDetails();
-                break;
-        }
-    }
 });
