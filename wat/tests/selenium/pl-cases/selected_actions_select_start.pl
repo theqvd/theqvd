a9cffc371313e87a1cc33d3e002e7cfd613dae15
Sergio Martin Zarzuelo <szarzuelo@qindel.com>
2015-10-26 11:10:14 +0000

    Update selenium tests according with latest massive actions changes

--- wat/tests/selenium/pl-cases/selected_actions_select_start.pl
+++
@@ -1,3 +0,0 @@
-$sel->click_at_ok("css=div.action-selected-select>div.chosen-container", "");
-my $actionIndex = $sel->get_element_index("css=select[name=\"selected_actions_select\"] option[value=\"start\"]");
-$sel->click_at_ok("css=div.action-selected-select>div.chosen-container .chosen-results [data-option-array-index=" . $actionIndex . "]", "");
