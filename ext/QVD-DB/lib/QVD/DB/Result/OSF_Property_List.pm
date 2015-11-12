c857da152f90b8644a1c3d69987bf4653522ef36
Francisco Trapero <ftrapero@qindel.com>
2015-11-12 12:36:55 +0000

    Bug #2019: Minor changes in BD for qvd demo
    Updated Properties to enable delete in cascade.

--- ext/QVD-DB/lib/QVD/DB/Result/OSF_Property_List.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/OSF_Property_List.pm
@@ -6,7 +6,7 @@ __PACKAGE__->table('osf_properties_list');
 __PACKAGE__->add_columns( property_id   => { data_type => 'integer' } );
 
 __PACKAGE__->set_primary_key('property_id');
-__PACKAGE__->has_many(properties => 'QVD::DB::Result::OSF_Property', 'property_id', { cascade_delete => 0 });
+__PACKAGE__->has_many(properties => 'QVD::DB::Result::OSF_Property', 'property_id', { cascade_delete => 1 });
 __PACKAGE__->belongs_to(properties_list => 'QVD::DB::Result::Property_List', 'property_id');
 
 sub tenant_name
--- ext/QVD-DB/lib/QVD/DB/Result/VM_Property_List.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/VM_Property_List.pm
@@ -6,7 +6,7 @@ __PACKAGE__->table('vm_properties_list');
 __PACKAGE__->add_columns( property_id   => { data_type => 'integer' } );
 
 __PACKAGE__->set_primary_key('property_id');
-__PACKAGE__->has_many(properties => 'QVD::DB::Result::VM_Property', 'property_id', { cascade_delete => 0 });
+__PACKAGE__->has_many(properties => 'QVD::DB::Result::VM_Property', 'property_id', { cascade_delete => 1 });
 __PACKAGE__->belongs_to(properties_list => 'QVD::DB::Result::Property_List', 'property_id');
 __PACKAGE__->belongs_to(properties_list => 'QVD::DB::Result::Property_List', 'property_id');
 
