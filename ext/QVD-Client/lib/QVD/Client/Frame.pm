32075ef10f891db0be15fa19bd58bb540602e45f
Vadim Troshchinskiy <vtroshchinskiy@qindel.com>
2015-12-11 13:55:04 +0000

    Fix startup warning

--- ext/QVD-Client/lib/QVD/Client/Frame.pm
+++ ext/QVD-Client/lib/QVD/Client/Frame.pm
@@ -685,7 +685,6 @@ sub OnUnknownCert {
             }
 
             my $hsizer = Wx::BoxSizer->new(wxHORIZONTAL);
-            my $excl = Wx::StaticText->new($info_panel, -1, "    !");
             my $excl = Wx::StaticText->new($info_panel, -1, "    " . chr(0x26A0));
 
             $font = $excl->GetFont();
