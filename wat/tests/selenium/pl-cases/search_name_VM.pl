e4d97150c09f3d8ce1b4ec8c973e4fa70967d0e5
Sergio Martin Zarzuelo <szarzuelo@qindel.com>
2016-01-07 09:38:49 +0000

    change selenium function to fill search field to a perl compatible one

--- wat/tests/selenium/pl-cases/search_name_VM.pl
+++ wat/tests/selenium/pl-cases/search_name_VM.pl
@@ -1,1 +1,2 @@
-$sel->send_keys_ok("css=input.desktop-filter[name=\"name\"]", "VM");
+$sel->type_ok("css=input.desktop-filter[name=\"name\"]", "VM");
+$sel->fire_event_ok("css=input.desktop-filter[name=\"name\"]", "input");
