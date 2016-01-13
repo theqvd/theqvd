1ed4af2f4ec103749e1783295d77bf9ceccbf730
Sergio Martin Zarzuelo <szarzuelo@qindel.com>
2016-01-13 09:24:35 +0000

    Improve detection of filter changes to avoid erratic fails on selenium tests

--- wat/tests/selenium/pl-cases/blocked_change_to_blocked.pl
+++ wat/tests/selenium/pl-cases/blocked_change_to_blocked.pl
@@ -3,7 +3,7 @@ $sel->click_at_ok("css=#filter_blocked_chosen", "");
 $sel->click_at_ok("css=div#filter_blocked_chosen .chosen-results [data-option-array-index=" . $actionIndex . "]", "");
 WAIT: {
     for (1..60) {
-        if (eval { $sel->is_element_present("css=div.loading-mid") }) { pass; last WAIT }
+        if (eval { $sel->is_element_present("css=a.js-delete-filter-note[data-filter-name=\"blocked\"]") }) { pass; last WAIT }
         sleep(1);
     }
     fail("timeout");
