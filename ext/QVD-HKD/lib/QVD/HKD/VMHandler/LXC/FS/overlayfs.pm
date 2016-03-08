e0eaa6cca635b89d51ac042f7cefdd585c1d3f2d
Salvador Fandino <salva@qindel.com>
2016-03-08 11:17:00 +0100

    Add support for recent versions of overlayfs requiring an additional working dir

--- ext/QVD-HKD/lib/QVD/HKD/VMHandler/LXC/FS/overlayfs.pm
+++ ext/QVD-HKD/lib/QVD/HKD/VMHandler/LXC/FS/overlayfs.pm
@@ -2,6 +2,7 @@ package QVD::HKD::VMHandler::LXC::FS::overlayfs;
 
 use strict;
 use warnings;
+use QVD::Log;
 
 use parent qw(QVD::HKD::VMHandler::LXC::FS);
 
@@ -13,12 +14,28 @@ sub init_backend {
 }
 
 sub _mount_root {
-    my ($self, $rootfs, $basefs, $overlayfs, $subdir) = @_;
+    my ($self, $rootfs, $basefs, $overlayfs, $subdir, $workdir) = @_;
     $basefs = File::Spec->join($basefs, $subdir) if defined $subdir;
     $self->_run_cmd({log_error => "Unable to mount overlayfs mix of '$basefs' (ro) and '$overlayfs' (rw) into '$rootfs'"},
                     'mount',
                     -t => 'overlayfs',
-                    -o => "rw,upperdir=$overlayfs,lowerdir=$basefs", "overlayfs", $rootfs);
+                    -o => "rw,upperdir=$overlayfs,lowerdir=$basefs,workdir=$workdir", "overlayfs", $rootfs);
+}
+
+sub _remove_old_workdir {
+    my $self = shift;
+    my $workdir = $self->{workdir};
+    -e $workdir or return $self->_on_done;
+    return $self->_move_dir($workdir, $self->{workdir_old});
+}
+sub _make_workdir {
+    my $self = shift;
+    my $workdir = $self->{workdir};
+    if (-e $workdir) {
+        ERROR "Work directory still exists at '$workdir' for VM $self->{vm_id}";
+        return $self->_on_error;
+    }
+    return $self->_make_dir($workdir, $self->{basefs});
 }
 
 1;
