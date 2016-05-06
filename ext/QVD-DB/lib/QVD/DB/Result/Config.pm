e7bc516970f2e6faa5ec64d0718f5e0eb6cb67bf
ftrapero <ftrapero@qindel.com>
2016-05-06 09:33:37 +0200

    Bug #2519: Changes to configuration are not notified to HKD
    Added trigger to generate the notification of any change in the configuration.

--- ext/QVD-DB/lib/QVD/DB/Result/Config.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/Config.pm
@@ -10,4 +10,86 @@ __PACKAGE__->add_columns( tenant_id => { data_type => 'integer' },
 __PACKAGE__->set_primary_key(qw(tenant_id key));
 __PACKAGE__->belongs_to(tenant => 'QVD::DB::Result::Tenant', 'tenant_id', { cascade_delete => 0 });
 
+####### TRIGGERS ############################################################################
+
+sub get_procedures
+{
+    my @procedures_array = ();
+
+    # Define procedures to listen and notify
+    my @notify_procs_array = (
+        {
+            name => 'config_changed_notify',
+            sql  => '$function$ BEGIN listen qvd_config_changed; PERFORM pg_notify(\'qvd_config_changed\', \'tenant_id=\' || NEW.tenant_id::text); RETURN NULL; END; $function$',
+            parameters => [],
+        },
+        {
+            name => 'config_created_notify',
+            sql  => '$function$ BEGIN listen qvd_config_changed; PERFORM pg_notify(\'qvd_config_changed\', \'tenant_id=\' || NEW.tenant_id::text); RETURN NULL; END; $function$',
+            parameters => [],
+        },
+        {
+            name => 'config_deleted_notify',
+            sql  => '$function$ BEGIN listen qvd_config_changed; PERFORM pg_notify(\'qvd_config_changed\', \'tenant_id=\' || OLD.tenant_id::text); RETURN NULL; END; $function$',
+            parameters => [],
+        },
+    );
+
+    for my $proc (@notify_procs_array){
+        $proc->{replace} = 1;
+        $proc->{language} = 'plpgsql';
+        $proc->{returns} = 'trigger';
+    }
+
+    push @procedures_array, @notify_procs_array;
+
+    # Return all procedures
+    return @procedures_array;
+}
+
+sub get_triggers
+{
+    my @triggers_array = ();
+
+    my @notify_triggers_array = (
+        {
+            name => 'config_changed_trigger',
+            when => 'AFTER',
+            events => [qw/UPDATE/],
+            fields    => [],
+            on_table  => 'configs',
+            condition => undef,
+            procedure => 'config_changed_notify',
+            parameters => [],
+            scope  => 'ROW',
+        },
+        {
+            name => 'config_created_trigger',
+            when => 'AFTER',
+            events => [qw/INSERT/],
+            fields    => [],
+            on_table  => 'configs',
+            condition => undef,
+            procedure => 'config_created_notify',
+            parameters => [],
+            scope  => 'ROW',
+        },
+        {
+            name => 'config_deleted_trigger',
+            when => 'AFTER',
+            events => [qw/DELETE/],
+            fields    => [],
+            on_table  => 'configs',
+            condition => undef,
+            procedure => 'config_deleted_notify',
+            parameters => [],
+            scope  => 'ROW',
+        },
+    );
+
+    push @triggers_array, @notify_triggers_array;
+
+    return @triggers_array;
+}
+
 1;
