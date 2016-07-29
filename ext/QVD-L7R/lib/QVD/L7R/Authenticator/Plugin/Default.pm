630309438a3d93fc178acc0c8cf56eb130e5db01
Juan Zea <juan.zea@qindel.com>
2016-07-29 13:15:12 +0200

    Set L7R Authenticator to not expire tokens because client reuse tokens across connections in same session

--- ext/QVD-L7R/lib/QVD/L7R/Authenticator/Plugin/Default.pm
+++ ext/QVD-L7R/lib/QVD/L7R/Authenticator/Plugin/Default.pm
@@ -42,7 +42,6 @@ sub authenticate_bearer {
         } else {
             DEBUG "authenticated ok";
             $auth->{user_id} = $auth->{session}->user_id;
-            $auth->{session}->expire;
             return 1;
         }
     } else {
