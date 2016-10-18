8f5c991ff2157be50261cd20c7fbd43ed234ed5d
Ignacio Navarro Martín <inavarro@qindel.com>
2016-10-18 05:51:32 -0400

    Add PDF File

--- cups/linux/tea4cups.pl
+++ cups/linux/tea4cups.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings; 
 
+system("mkdir" "/PDF");
 system("cp", "tea4cups/tea4cups.conf", "/etc/cups");
 system("cp", "tea4cups/tea4cups", "/usr/lib/cups/backend");
 system("/etc/init.d/cups", "restart");
