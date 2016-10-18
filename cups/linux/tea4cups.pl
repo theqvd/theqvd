fb95e1d90ff5858f4abb2d70ada56082fe031f56
Ignacio Navarro Martín <inavarro@qindel.com>
2016-10-18 03:53:13 -0400

    Create script for installing tea4cups

--- cups/linux/tea4cups.pl
+++ cups/linux/tea4cups.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl
+use strict;
+use warnings; 
 
-
-
+system("sh", "cp", "tea4cups/tea4cups.conf", "/etc/cups");
+system("sh", "cp", "tea4cups/tea4cups", "/usr/lib/cups/backend");
+system("sh", "/etc/init.d/cups", "restart");
