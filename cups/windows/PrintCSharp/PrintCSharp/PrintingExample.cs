cac13ab031edb571bee03de1e360a50ab5d14e45
Ignacio Navarro <inavarro@qindel.com>
2016-10-20 10:50:05 +0200

    Add windows project

---
+++ cups/windows/PrintCSharp/PrintCSharp/PrintingExample.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace PrintCSharp
+{
+    class PrintingExample
+    {
+        private Font printFont;
+        private StreamReader streamToPrint;
+
+        // The Click event is raised when the user clicks the Print button.
+        public void StartPrint(string printerName, string filePath)
+        {
+            Console.WriteLine();
+            try
+            {
+                streamToPrint = new StreamReader(filePath);
+
+                try
+                {
+                    printFont = new Font("Arial", 10);
+                    PrintDocument pd = new PrintDocument();
+                    pd.PrintPage += new PrintPageEventHandler(this.PrintPage);
+                    pd.PrinterSettings.PrinterName = printerName;
+
+                    if(pd.PrinterSettings.IsValid)
+                    {
+                        pd.Print();
+                    } else
+                    {
+                        Console.WriteLine("Printer not valid");
+                    }
+                }
+                finally
+                {
+                    streamToPrint.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.ToString());
+            }
+        }
+
+        // The PrintPage event is raised for each page to be printed.
+        private void PrintPage(object sender, PrintPageEventArgs ev)
+        {
+
+            float linesPerPage = 0;
+            float yPos = 0;
+            int count = 0;
+            float leftMargin = ev.MarginBounds.Left;
+            float topMargin = ev.MarginBounds.Top;
+            string line = null;
+
+            // Calculate the number of lines per page.
+            linesPerPage = ev.MarginBounds.Height /
+               printFont.GetHeight(ev.Graphics);
+
+            // Print each line of the file.
+            while (count < linesPerPage &&
+               ((line = streamToPrint.ReadLine()) != null))
+            {
+                yPos = topMargin + (count *
+                   printFont.GetHeight(ev.Graphics));
+                ev.Graphics.DrawString(line, printFont, Brushes.Black,
+                   leftMargin, yPos, new StringFormat());
+                count++;
+            }
+
+            // If more lines exist, print another page.
+            if (line != null)
+                ev.HasMorePages = true;
+            else
+                ev.HasMorePages = false;
+
+    
+        }
+
+    }
+}
