868ef5f29828aa920c2343282aeaed8890640b82
ftrapero <ftrapero@qindel.com>
2016-11-22 17:34:59 +0100

    Bug #3089: Enable delete cascade when deleting roles that inherits from others

--- ext/QVD-DB/lib/QVD/DB/Result/Role.pm
+++ ext/QVD-DB/lib/QVD/DB/Result/Role.pm
@@ -20,7 +20,7 @@ __PACKAGE__->add_unique_constraint(['name', 'tenant_id']);
 __PACKAGE__->belongs_to(tenant => 'QVD::DB::Result::Tenant',  'tenant_id', { cascade_delete => 0 });
 __PACKAGE__->has_many(admin_rels => 'QVD::DB::Result::Role_Administrator_Relation', 'role_id', { cascade_delete => 0 } );
 __PACKAGE__->has_many(acl_rels => 'QVD::DB::Result::ACL_Role_Relation', 'role_id');
-__PACKAGE__->has_many(role_rels => 'QVD::DB::Result::Role_Role_Relation', 'inheritor_id', { cascade_delete => 0 } );
+__PACKAGE__->has_many(role_rels => 'QVD::DB::Result::Role_Role_Relation', 'inheritor_id', { cascade_delete => 1 } );
 __PACKAGE__->has_many(parent_role_rels => 'QVD::DB::Result::Role_Role_Relation', 'inherited_id', { cascade_delete => 0 } );
 
 
