3dbd9e5fec346e84b371d32d6f109cfa01f15388
Sergio M. Zarzuelo <szarzuelo@qindel.com>
2017-01-25 14:50:10 +0000

    Ticket #3717: Add support to UP API for client type attribute for workspaces and desktops

--- ext/QVD-DB/lib/QVD/DB/Common.pm
+++ ext/QVD-DB/lib/QVD/DB/Common.pm
@@ -22,7 +22,7 @@ sub ENUMERATES {
         language_enum                                          =>
         [ qw(es en auto default) ],
         user_portal_parameters_enum                            =>
-        [ qw(connection audio printers fullscreen share_folders share_usb) ],
+        [ qw(connection audio printers fullscreen share_folders share_usb client) ],
     };
 }
 
