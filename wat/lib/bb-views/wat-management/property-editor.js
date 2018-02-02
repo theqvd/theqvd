a3bc5e7f45440f7107946f6593783834e81654e2
Zarzuelo <szarzuelo@qindel.com>
2018-02-02 13:26:40 +0100

    Fix styles, dictionary and property functions location after merge

--- wat/lib/bb-views/wat-management/property-editor.js
+++ wat/lib/bb-views/wat-management/property-editor.js
@@ -14,13 +14,53 @@ Wat.Views.PropertyEditorView = Wat.Views.EditorView.extend({
         Wat.CurrentView.model = new Wat.Models.Property();
         $('.ui-dialog-titlebar').html($.i18n.t('New property'));
         
+        // When admin is a superadmin, the change of the tenant selector will trigger the property objects renderization
+        if (Wat.C.isSuperadmin()) {
+            Wat.B.bindEvent('change', '[name="tenant_id"]', this.renderPropertiesObjects);
+        }
+        
         Wat.Views.EditorView.prototype.renderCreate.apply(this, [target, that]);
+        
+        // When admin is not a superadmin, the property objects will be renderized once
+        if (!Wat.C.isSuperadmin()) {
+            this.renderPropertiesObjects();
+        }
     },
     
     renderUpdate: function (target, that) {
         Wat.Views.EditorView.prototype.renderUpdate.apply(this, [target, that]);
         
         $('.ui-dialog-titlebar').html($.i18n.t('Edit property'));
+    },
+    
+    renderPropertiesObjects: function () {
+        // Host properties are only allowed to be managed by superadministrators in supertenant
+        var hostPropertiesEnabled = Wat.C.isSuperadmin() && $('select[name="tenant_id"]').val() == 0;
+        
+        // Objects editor have checked the filtered objects initially
+        var selectedObj = $('select[name="obj-qvd-select"]').val();
+        var checkedObjs = {
+            user: selectedObj == 'all',
+            vm: selectedObj == 'all',
+            host: selectedObj == 'all',
+            osf: selectedObj == 'all',
+            di: selectedObj == 'all'
+        };
+        
+        if (selectedObj != 'all') {
+            checkedObjs[selectedObj] = true;
+        }
+        
+        var template = _.template(
+            Wat.TPL.editorPropertyObjects, {
+                hostPropertiesEnabled: hostPropertiesEnabled,
+                checkedObjs: checkedObjs
+            }
+        );
+        
+        $('.js-editor-container[data-qvd-obj="property"] .bb-editor-extra').html(template);
+        
+        Wat.T.translate();
     },
     
     createElement: function () {
--- wat/lib/bb-views/wat-management/property.js
+++ wat/lib/bb-views/wat-management/property.js
@@ -246,50 +246,6 @@ Wat.Views.PropertyView = Wat.Views.MainView.extend({
         Wat.Views.MainView.prototype.openEditElementDialog.apply(this, [e]);
     },
     
-    renderPropertiesObjects: function () {
-        // Host properties are only allowed to be managed by superadministrators in supertenant
-        var hostPropertiesEnabled = Wat.C.isSuperadmin() && $('select[name="tenant_id"]').val() == 0;
-        
-        // Objects editor have checked the filtered objects initially
-        var selectedObj = $('select[name="obj-qvd-select"]').val();
-        var checkedObjs = {
-            user: selectedObj == 'all',
-            vm: selectedObj == 'all',
-            host: selectedObj == 'all',
-            osf: selectedObj == 'all',
-            di: selectedObj == 'all'
-        };
-        
-        if (selectedObj != 'all') {
-            checkedObjs[selectedObj] = true;
-        }
-        
-        var template = _.template(
-            Wat.TPL.editorPropertyObjects, {
-                hostPropertiesEnabled: hostPropertiesEnabled,
-                checkedObjs: checkedObjs
-            }
-        );
-        
-        $('.js-editor-container[data-qvd-obj="property"] .bb-editor-extra').html(template);
-        
-        Wat.T.translate();
-    },
-    
-    fillEditor: function (target, that) {
-        // When admin is a superadmin, the change of the tenant selector will trigger the property objects renderization
-        if (Wat.C.isSuperadmin()) {
-            Wat.B.bindEvent('change', '[name="tenant_id"]', that.renderPropertiesObjects);
-        }
-        
-        
-        Wat.Views.MainView.prototype.fillEditor.apply(this, [target, that]);
-        // When admin is not a superadmin, the property objects will be renderized once
-        if (!Wat.C.isSuperadmin()) {
-            that.renderPropertiesObjects();
-        }
-    },
-    
     askDelete: function (e) {
         var propertyId = $(e.target).attr('data-property-id');
         
