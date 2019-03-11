074f2c00dec7a093b68868d07a408137360dab1f
jzea <juan.zea@qindel.com>
2019-03-11 09:23:35 +0100

    Task #7186: Implemented Helper and took unbind out of Frame.pm

--- ext/QVD-Config-Core/lib/QVD/Config/Core/Defaults.pm
+++ ext/QVD-Config-Core/lib/QVD/Config/Core/Defaults.pm
@@ -424,6 +424,9 @@ client.usb.share_all = 0
 # 0441:0012, 1234:5678@12345678
 client.usb.share_list =
 
+# QVD Client Slaveclient command
+client.slave.command.qvd-client-slaveclient-usbip=${path.qvd.bin}/qvd-client-slaveclient-usbip
+
 ## display kill vm session checkbox
 client.kill_vm.display = 0
 
